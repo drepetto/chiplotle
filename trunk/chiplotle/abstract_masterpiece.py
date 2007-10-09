e617fa8f944649e313bf246f3f673ba72e0f0a5d
douglas <douglas@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2007-10-09 22:05:31 +0000

    added an abstract shape drawing routine.

--- trunk/chiplotle/abstract_masterpiece.py
+++ trunk/chiplotle/abstract_masterpiece.py
@@ -5,26 +5,23 @@ from languages import chiplotle_hpgl as chpgl
 
 c = run_chiplotle_UNIX.c
 
+width = c.output.right() - c.output.left()
+height = c.output.top() - c.output.bottom()
+
+print "width: %d height: %d" % (width, height)
+
 pens = raw_input("\nhow many pens do you want to use? ")
 numPens = int(pens)
 
 #start in a random spot
 c.eat = chpgl.goto(random.randint(c.output.left(), c.output.right()), random.randint(c.output.bottom(), c.output.top()))
 
-# how many gestures do we wnat to do?
-
-#numGestures = random.randint(2,20)
-
-#for gesture in range(numGestures):
-
 penNum = 1
 
 while True:
-    #print "gesture %d:" % gesture
-
     c.eat = chpgl.sp(penNum)
 
-    whichGesture = random.randint(0,4)
+    whichGesture = random.randint(0,5)
     
     if whichGesture == 0:
         print "circle!"
@@ -58,6 +55,23 @@ while True:
         c.eat = chpgl.pu()
         
     elif whichGesture == 4:
+        print "draw an abstract shape!"
+        numPoints = random.randint(2,4)
+        print "numPoints: ", numPoints
+        firstX = random.randint(c.output.left(), c.output.right())
+        firstY = random.randint(c.output.bottom(), c.output.top())
+        c.eat = chpgl.goto(firstX, firstY)
+        c.eat = chpgl.pd()
+        xRange = width/5
+        yRange = height/5
+                
+        for i in range(numPoints):
+            #c.eat = chpgl.goto(random.randint(c.output.left(), c.output.right()), random.randint(c.output.bottom(), c.output.top()))
+            c.eat = chpgl.nudge(random.randint(-xRange, xRange), random.randint(-yRange, yRange))
+        c.eat = chpgl.goto(firstX, firstY)
+        c.eat = chpgl.pu()
+        
+    elif whichGesture == 5:
         print "just jump around!"
         c.eat = chpgl.goto(random.randint(c.output.left(), c.output.right()), random.randint(c.output.bottom(), c.output.top()))
         
