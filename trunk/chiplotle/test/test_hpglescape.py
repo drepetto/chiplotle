5bac3cdaad576f73fba56c2f43b7acd38a16ee81
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2008-12-12 05:57:58 +0000

    Fixed bug in the _HPGLEscape class '_escape' attribute setter in the exact same way as we fixed the setter for the '_terminator' class attribute of the _HPGLCommand class. Added test_hpglescap.py test file.

---
+++ trunk/chiplotle/test/test_hpglescape.py
@@ -0,0 +1,13 @@
+from chiplotle import *
+
+def test_hpglescape_escape_01( ):
+   '''The escape class attribute of _HPGLEscape commands can be set directly
+   from the instances.'''
+   on = On( )
+   off = Off( )
+   on.escape = chr(1)
+   assert on.escape == chr(1)
+   assert off.escape == chr(1)
+   off.escape = chr(2)
+   assert on.escape == chr(2)
+   assert off.escape == chr(2)
