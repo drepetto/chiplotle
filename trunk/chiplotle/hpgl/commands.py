c9b95337021530b3d83f7fe16d3d0a351dde89d9
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2008-12-12 20:32:54 +0000

    Removed unnecessary 'pass'(es) from commands.py

--- trunk/chiplotle/hpgl/commands.py
+++ trunk/chiplotle/hpgl/commands.py
@@ -48,14 +48,14 @@ class CC(_HPGLCommand):
 
 class AF(_HPGLCommand):
    '''Advance full page.'''
-   pass
+   
 
 class AH(_HPGLCommand):
    '''Advance half page.'''
-   pass
+   
 
 class AP(_HPGLCommand):
-   """
+   '''
       Automatic Pen operations
       for 7550:
       bit_no dec_val state meaning
@@ -70,7 +70,7 @@ class AP(_HPGLCommand):
 
       default is 7 on 7550
       codes are 0 to 255 with default of 95 on the DraftMaster
-   """
+   '''
    def __init__(self, p=''):   
       if p != '':
          self.p = int(p)
@@ -92,13 +92,13 @@ class AA(_Arc):
       _Arc.__init__(self, xy, a, chordangle, True)
 
 class AS(_HPGLCommand):
-   """
+   '''
       Acceleration Select
       Can be set per-pen or for all pens at once.
 
       default on 7550: 6
       default on DM: 4
-   """
+   '''
    def __init__(self, accel=None, pen=None):   
       self.accel = accel
       self.pen = pen
@@ -115,23 +115,23 @@ class AS(_HPGLCommand):
          return '%s%s' % (self._name, self.terminator) 
 
 class EA(_Positional):
-   """Edge Rectangle Absolute."""
+   '''Edge Rectangle Absolute.'''
    def __init__(self, xy):
       assert type(xy) in (tuple, list) and len(xy) == 2
       _Positional.__init__(self, xy, True)
 
 class ER(_Positional):
-   """Edge Rectangle Relative."""
+   '''Edge Rectangle Relative.'''
    def __init__(self, xy):
       assert type(xy) in (tuple, list) and len(xy) == 2
       _Positional.__init__(self, xy, False)
 
 class FS(_HPGLCommand):
-   """Set tip force for pen. 
+   '''Set tip force for pen. 
       force range is 1-8
       pen range is 1-8
       if pen == None then all pens are set to force
-   """
+   '''
    def __init__(self, force = None, pen = None):
       self.force = force
       self.pen = pen
@@ -148,24 +148,24 @@ class FS(_HPGLCommand):
 
 
 class RA(_Positional):
-   """Filled Rectangle Absolute."""
+   '''Filled Rectangle Absolute.'''
    def __init__(self, xy):
       assert type(xy) in (tuple, list) and len(xy) == 2
       _Positional.__init__(self, xy, True)
 
 class RR(_Positional):
-   """Filled Rectangle Relative."""
+   '''Filled Rectangle Relative.'''
    def __init__(self, xy):
       assert type(xy) in (tuple, list) and len(xy) == 2
       _Positional.__init__(self, xy, False)
 
 class VS(_HPGLCommand):
-   """ 
+   ''' 
       Pen Velocity.
       v valid range: 0.0-127.9999 (depends on plotter)
       default depends on plotter and carousel type
       pen valid range: 1-8
-   """
+   '''
    def __init__(self, *args):
       ### this can be simpler/cleaner 
       self.vel = self.pen = None
@@ -194,55 +194,55 @@ class EP(_HPGLCommand):
 
 class BF(_HPGLCommand):
    '''Buffer plot.'''
-   pass
+   
 
 class DC(_HPGLCommand):
    '''Clear Digitizer.'''
-   pass
+   
 
 class DF(_HPGLCommand):
    '''Default Instructions.'''
-   pass
+   
 
 class DP(_HPGLCommand):
    '''Digitize Points.'''
-   pass
+   
 
 class FP(_HPGLCommand):
    '''Fill Polygon.'''
-   pass
+   
 
 class FR(_HPGLCommand):
    '''Advance Frame.'''
-   pass
+   
 
 class NR(_HPGLCommand):
    '''Not Ready.'''
-   pass
+   
 
 class OA(_HPGLCommand):
-   """
+   '''
       Returns current actual position of pen.
       X, Y, P (0 = PU, 1 = PD)
-   """
-   pass
+   '''
+   
 
 class OC(_HPGLCommand):
-   """
+   '''
       Returns commanded position of pen.
       X, Y, P (0 = PU, 1 = PD)
-   """
-   pass
+   '''
+   
 
 class OD(_HPGLCommand):
-   """
+   '''
       Returns last digitized point.
       X, Y, P (0 = PU, 1 = PD)
-   """
-   pass
+   '''
+   
 
 class OE(_HPGLCommand):
-   """
+   '''
       Return first HP-GL error.
       #'s 0-8, excluding 4 and 7
       
@@ -259,42 +259,42 @@ class OE(_HPGLCommand):
       
       
       NOTE: some error meanings change depending on the plotter!
-   """
-   pass
+   '''
+   
 
 class OF(_HPGLCommand):
-   """
+   '''
       dog ass me.
       Always outputs '40,40', which means that there are 40 plotter units/mm
-   """
-   pass
+   '''
+   
 
 class OH(_HPGLCommand):
-   """Return hard limits of plotter"""
-   pass
+   '''Return hard limits of plotter'''
+   
 
 class OI(_HPGLCommand):
-   """Get ID of plotter."""
-   pass
+   '''Get ID of plotter.'''
+   
 
 class OK(_HPGLCommand):
    '''Output key.'''
-   pass
+   
 
 class OL(_HPGLCommand):
    '''Output label length.'''
-   pass
+   
 
 class OO(_HPGLCommand):
    '''Output options.'''
-   pass
+   
 
 class OP(_HPGLCommand):
    '''Get P1 & P2.'''
-   pass
+   
 
 class OS(_HPGLCommand):
-   """
+   '''
       Return plotter status.
       
       bit value   bit position   meaning
@@ -310,23 +310,23 @@ class OS(_HPGLCommand):
       power-on status == 24 (bits 3 & 4 set)
       
       but these may be different on different plotters...
-   """
-   pass
+   '''
+   
 
 class OT(_HPGLCommand):
    '''Output carousel type.'''
-   pass
+   
 
 class OW(_HPGLCommand):
-   """
+   '''
      Output window.
      Return xLL, yLL, xUR, yUR in plotter coords.
-   """
-   pass
+   '''
+   
 
 class PB(_HPGLCommand):
    '''Print buffered label.'''
-   pass
+   
       
 class PS(_HPGLCommand):
    '''Change the size of the hard clip limits.'''
@@ -345,10 +345,10 @@ class PS(_HPGLCommand):
          return '%s%s' % (self._name, self.terminator)
 
 class BL(_HPGLCommand):
-   """
+   '''
       Stores first 150 chars of text in buffer for later printing
       and label length measurements.
-   """
+   '''
    def __init__(self, label=None):
       self.label = label
 
@@ -361,20 +361,20 @@ class BL(_HPGLCommand):
 
 class IN(_HPGLCommand):
    '''Initialize.'''
-   pass
+   
 
 
 class SS(_HPGLCommand):
    '''Select standard character set.'''
-   pass
+   
 
 class XT(_HPGLCommand):
    '''X tick.'''
-   pass
+   
 
 class YT(_HPGLCommand):
    '''Y tick.'''
-   pass
+   
 
 class CS(_HPGLCommand):
    '''Character set.'''
@@ -414,11 +414,11 @@ class CM(_HPGLCommand):
       return '%s%d,%d%s' % (self._name, self.switch, self.fallback, self.terminator)
 
 class CP(_HPGLCommand):
-   """
+   '''
       Move the pen the specified number of spaces and lines
       valid values are -128 to 128
       CP by itself does CR & LF
-   """
+   '''
    def __init__(self, spaces=None, lines=None):   
       self.spaces = spaces
       self.lines = lines
@@ -436,7 +436,7 @@ class CP(_HPGLCommand):
          return '%s%s' % (self._name, self.terminator)
 
 class DT(_HPGLCommand):
-   """Define Label terminator."""
+   '''Define Label terminator.'''
    def __init__(self, terminator = chr(3)):   
       self.labelTerminator = terminator
 
@@ -445,7 +445,7 @@ class DT(_HPGLCommand):
       return '%s%c%s' % (self._name, self.labelTerminator, self.terminator)
 
 class LB(_HPGLCommand):
-   """Print text 'label'."""
+   '''Print text 'label'.'''
    def __init__(self, text):   
       self.text = text
       self.labelTerminator = chr(3)
@@ -455,7 +455,7 @@ class LB(_HPGLCommand):
       return '%s%s%s%s' % (self._name, self.text, self.labelTerminator, self.terminator)
 
 class SP(_HPGLCommand):
-   """Select Pen."""
+   '''Select Pen.'''
    def __init__(self, pen = 0):   
       self.pen = pen
 
@@ -464,7 +464,7 @@ class SP(_HPGLCommand):
       return '%s%d%s' % (self._name, self.pen, self.terminator)
 
 class LT(_HPGLCommand):
-   """Define line type:  
+   '''Define line type:  
    0:  plot point at given point.
    1:  .   .   .   .   .   .
    2:  __   __   __   __   __
@@ -472,7 +472,7 @@ class LT(_HPGLCommand):
    4:  __.__.__.__.__.__.
    5:  ___ _ ___ _ ___ _ ___ _
    6:  ___ _ _ ___ _ _ ___ _ _ ___
-   """
+   '''
    def __init__(self, pattern=None, length=4):   
       self.pattern = pattern
       self.length = length
@@ -486,12 +486,12 @@ class LT(_HPGLCommand):
          return '%s%s' % (self._name, self.terminator)
 
 class FT(_HPGLCommand):
-   """ Set fill type:
+   ''' Set fill type:
    1:  Solid (space and angle ignored)
    2:  Solid too? (space and angle ignored)
    3:  Hatching
    4:  Cross hatching
-   """
+   '''
    def __init__(self, type = 1, space=None, angle=0):   
       self.type = type
       self.space = space
@@ -506,7 +506,7 @@ class FT(_HPGLCommand):
          return '%s%d%s' % (self._name, self.type, self.terminator)
 
 class PM(_HPGLCommand):
-   """Plot polygon."""
+   '''Plot polygon.'''
    def __init__(self, n = 0):   
       self.n = n
 
@@ -515,7 +515,7 @@ class PM(_HPGLCommand):
       return '%s%d%s' % (self._name, self.n, self.terminator)
 
 class EC(_HPGLCommand):
-   """Enable cut line."""
+   '''Enable cut line.'''
    def __init__(self, n = 0):   
       self.n = n
 
@@ -524,7 +524,7 @@ class EC(_HPGLCommand):
       return '%s%d%s' % (self._name, self.n, self.terminator)
 
 class PG(_HPGLCommand):
-   """Page feed."""
+   '''Page feed.'''
    def __init__(self, n = None):   
       self.n = n
 
@@ -536,7 +536,7 @@ class PG(_HPGLCommand):
          return '%s%s' % (self._name, self.terminator)
 
 class SL(_HPGLCommand):
-   """ Character Slant. Argument is tan of desired angle.  """
+   ''' Character Slant. Argument is tan of desired angle.  '''
    def __init__(self, tan = 0):   
       self.tan = tan
 
@@ -546,10 +546,10 @@ class SL(_HPGLCommand):
 
 class SA(_HPGLCommand):
    '''Select alternate character set.'''
-   pass
+   
 
 class RO(_HPGLCommand):
-   """ Rotate coordinate system.  """
+   ''' Rotate coordinate system.  '''
    def __init__(self, angle = 0):   
       self.angle = angle
 
@@ -558,7 +558,7 @@ class RO(_HPGLCommand):
       return '%s%d%s' % (self._name, self.angle, self.terminator)
 
 class RP(_HPGLCommand):
-   """ Replot.  """
+   ''' Replot.  '''
    def __init__(self, n = 1):   
       self.n = n
 
@@ -567,10 +567,10 @@ class RP(_HPGLCommand):
       return '%s%d%s' % (self._name, self.n, self.terminator)
 
 class SM(_HPGLCommand):
-   """ Symbol Mode.  Plots the char at each plotted point. 
+   ''' Symbol Mode.  Plots the char at each plotted point. 
        char can be any printing ascii char, except ';'
        Calling without an argument cancels symbol mode.
-   """
+   '''
    def __init__(self, char = None):
       self.char = char
 
@@ -582,22 +582,22 @@ class SM(_HPGLCommand):
          return '%s%s' % (self._name, self.terminator)
 
 class SC(_TwoPoint):
-   """Scale.
+   '''Scale.
       NOTE: DraftMaster also has a more complex version 
       of 'SC' that is not implemented yet...
-   """
+   '''
    def __init__(self, coords=None):
      _TwoPoint.__init__(self, coords) 
 
 
 class IW(_TwoPoint):
-   """ Set plotting window.  """
+   ''' Set plotting window.  '''
    def __init__(self, coords=None):
       _TwoPoint.__init__(self, coords) 
 
 
 class IP(_TwoPoint):
-   """Set P1 & P2 scaling points"""
+   '''Set P1 & P2 scaling points'''
    def __init__(self, coords=None):   
       if coords:
          assert len(coords) in (2, 4)
@@ -605,10 +605,10 @@ class IP(_TwoPoint):
 
 
 class PT(_HPGLCommand):
-   """
+   '''
       Pen Thickness
       0.1mm < thickness < 5.0mm
-   """
+   '''
    def __init__(self, thickness = 0.3):
       self.thickness = thickness
 
@@ -618,13 +618,13 @@ class PT(_HPGLCommand):
       
 
 class SI(_CharSize):
-   """Absolute character size.
-      Default values are width = 0.285cm, height=0.375cm"""
-   pass
+   '''Absolute character size.
+      Default values are width = 0.285cm, height=0.375cm'''
+   
 
 class SR(_HPGLCommand):
    '''Relative character size.'''
-   pass
+   
       
 class DI(_HPGLCommand):
    '''Absolute direction of label.'''
@@ -730,30 +730,30 @@ class WD(_HPGLCommand):
 
 class B(_HPGLEscape):
    '''Escape output buffer space.'''
-   pass
+   
 
 class On(_HPGLEscape):
-   """
+   '''
       Places the plotter in a programmed on-state.
       
       old description (from where?):
       Instructs the plotter to interpret data as HPGL and DCI instructions, 
       rather than plotting the data stream as literal text characters.
-   """
+   '''
    @property
    def _name(self):
       return '('
    
 class Off(_HPGLEscape):
-   """
+   '''
       Places the plotter in a programmed off-state
-   """
+   '''
    @property
    def _name(self):
       return ')'
 
 class ExtendedError(_HPGLEscape):
-   """
+   '''
       Get RS-232-C related error message.
       0 == no error
       10-16 == error
@@ -768,13 +768,13 @@ class ExtendedError(_HPGLEscape):
       15        framing, parity, or overrun error
       16        input buffer overflow
       
-   """
+   '''
    @property
    def _name(self):
       return 'E'
 
 class K(_HPGLEscape):
-   """Abort command: Tells the plotter to discard commands in its buffer."""
-   pass
+   '''Abort command: Tells the plotter to discard commands in its buffer.'''
+   
 
 
