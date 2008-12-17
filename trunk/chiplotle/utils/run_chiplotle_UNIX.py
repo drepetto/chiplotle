ae3d81c9aa330493fd6751fbe9ef38e3a07b3d53
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2008-12-17 05:29:01 +0000

    Moved open_port( ) defined in run_chiplotle_UNIX.py helper to its own file under utils and renamed to open_serial( ).

--- trunk/chiplotle/utils/run_chiplotle_UNIX.py
+++ trunk/chiplotle/utils/run_chiplotle_UNIX.py
@@ -6,35 +6,13 @@
 """
 
 from chiplotle import *
-#from plotters import *
+from chiplotle.utils.open_serial import open_serial
+
 
 print "\n* * * CHIPLOTLE in the house! * * *\n"
 
-def open_port( ):
-   import os
-   import re
-   import serial
-   # get available ports
-   availablePorts = os.listdir("/dev")
-   ttyTest = re.compile("tty")
-   ttys = filter(ttyTest.search, availablePorts)   
-
-   print "Available ports:\r"
-   for i,port in enumerate(ttys):
-      print "[%d] %s\t" % (i, port),
-      if i % 5 == 4:
-         print '\r'
-
-   sp = raw_input("\n\nChoose serial port [0 - %d]: " % (i - 1))
-
-   print "okay, opening %s..." % ttys[int(sp)]
-   ser = serial.Serial("/dev/" + ttys[int(sp)], 9600, timeout=.1)
-   return ser
-
-ser = open_port( )
+ser = open_serial( )
 print ser
-
-# ------------------------
 
 p = plotters.Plotter(ser)
 print "\nPlotter with ID %s found in selected port." % p.id
