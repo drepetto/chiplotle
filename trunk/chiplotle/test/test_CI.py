166122b372a816401f334a7f558dc389fef2182d
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2008-12-19 20:01:55 +0000

    Added tests to test_CI.py

--- trunk/chiplotle/test/test_CI.py
+++ trunk/chiplotle/test/test_CI.py
@@ -10,6 +10,7 @@ def test_CI_02( ):
    '''CI can take only radius argument.'''
    t = CI(1)
    assert t.radius == 1
+   assert isinstance(t.radius, Scalable)
 
 def test_CI_03( ):
    '''CI takes at most 2 arguments: radius and chord angle.'''
@@ -18,4 +19,17 @@ def test_CI_03( ):
    assert t.chordangle == 90
    assert t.format == 'CI1.0000,90;'
 
+### RADIUS ###
 
+def test_CI_radius_01( ):
+   '''Radius is always a Scalable.'''
+   t = CI(1)
+   assert isinstance(t.radius, Scalable)
+   t.radius = 1
+   assert isinstance(t.radius, Scalable)
+   t.radius = 1.
+   assert isinstance(t.radius, Scalable)
+
+#def test_CI_radius_02( ):
+#   '''Radius must be scalar.'''
+#   assert raises(TypeError, 't = CI([1])')
