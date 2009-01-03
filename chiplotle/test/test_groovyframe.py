cca503ece7ed9716f6755534f8c3a4ff44d67785
victor <vctradn@gmail.com>
2009-01-03 19:20:45 +0000

    Added GroovyFrame compound HPGL class and test_groovyframe.py. GroovyFrame will connect two rectangles with a given number of straight lines.

---
+++ chiplotle/test/test_groovyframe.py
@@ -0,0 +1,20 @@
+from chiplotle import *
+from py.test import raises
+
+def test_groovyframe_01( ):
+   '''
+   GroovyFrame must be initialized with a pair of xy coordinates,
+   a pair of width and height for square 1, a pair of width height
+   coordinates for square 2 and line count scalar.
+   '''
+   t = GroovyFrame((0, 0), (100, 100), (110, 110), 4)
+   assert t.wh1 == (100, 100)
+   assert t.wh2 == (110, 110)
+   assert t.linesPerSide == 4
+   assert all(t.xy == (0, 0))
+
+def test_groovyframe_02( ):
+   '''Width and height of rectangles must be a pair.'''
+   assert raises(AssertionError, 't = GroovyFrame((0, 0), 100, 110, 4)')
+   assert raises(AssertionError, 't=GroovyFrame((0, 0), 100, (110, 110), 4)')
+   assert raises(AssertionError, 't=GroovyFrame((0, 0), (100, 100), 110, 4)')
