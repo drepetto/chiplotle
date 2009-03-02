861925b272843e04df5b2a46d344dc389fc78e62
victor <vctradn@gmail.com>
2009-03-02 17:02:57 +0000

    Added scr folder with scripts to build documentation. scr/hpgl_classes.py creates the hpgl_classes chapter. Run this everytime the hpgl/commands.py source file changes to update docs. scr/wrap_html.py wraps the content of each text.html file in the chapters subdirectory with an HTML menu and header, and writes an index.html file. scr/rebuild.py runs wrap_html.py on all chapters.
    To add documentation, write straight HTML to a text.html file inside a chapters subdirectory and then run the rebuild.py script.

---
+++ chiplotle/documentation/scr/hpgl_classes.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+from chiplotle.cfg.cfg import CHIPLOTLE_DIR
+from chiplotle.hpgl import commands
+#from chiplotle.documentation.templates.html import *
+import os
+
+output_file_path = os.sep.join([CHIPLOTLE_DIR, 'documentation', 'chapters', 'hpgl_classes', 'text.html'])
+
+commands_output_file = open(output_file_path, 'w')
+
+contents = [ ]
+classes = dir(commands)
+for name in classes:
+   if not name.startswith('_'):
+      doc = eval('commands.%s.__doc__' % name)
+      if doc:
+         doc = doc.split('\n')
+         doc = '<br>'.join(doc[1:])
+         text = '<h3>%s</h3><p>%s</p>' % (name, doc) 
+         contents.append(text)
+
+title = '<h1>HP-GL classes</h1>'
+contents.insert(0, title)
+contents_str = '\n'.join(contents)
+commands_output_file.write(contents_str)
+commands_output_file.close( )
