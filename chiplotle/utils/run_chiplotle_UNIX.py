7df9733d8f46bedc0cb62b00da355df9e6de8479
victor <vctradn@gmail.com>
2009-03-28 16:54:37 +0000

    Removed commented lines from run_chiplotle_UNIX.py

--- chiplotle/utils/run_chiplotle_UNIX.py
+++ chiplotle/utils/run_chiplotle_UNIX.py
@@ -1,21 +1,11 @@
-"""
- *  This file is part of chiplotle.
- *
- *  http://music.columbia.edu/cmc/chiplotle
-"""
-
 from chiplotle import *
 from chiplotle.utils.interactive_open_serial import interactive_open_serial
 from chiplotle.utils.interactive_choose_plotter import interactive_choose_plotter
 from chiplotle.cfg.read_config_value import read_config_value
-#from chiplotle.cfg.get_serial_port import get_serial_port
-#from chiplotle.cfg.verify_config_file import verify_config_file
 import serial
 
 print "\n* * * CHIPLOTLE in the house! * * *\n"
 
-#verify_config_file( )
-#port = get_serial_port( )
 ## serial port
 port = read_config_value('serial_port')
 if port:
