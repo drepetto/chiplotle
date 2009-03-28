5531764607e46d11be1cbb2a7a5a05cdc7a9dcaf
victor <vctradn@gmail.com>
2009-03-28 15:38:33 +0000

    Added general reading and writing functions for handling the `config` configuration file. This will allow us to add new configuration parameters without having to write new code. Also refactored the run_chiplotle_UNIX.py helper and created a new interactive_choose_plotter( ) helper function. This commit is not tested. Will test at CMC and will tidy up this commit soon.

--- chiplotle/utils/run_chiplotle_UNIX.py
+++ chiplotle/utils/run_chiplotle_UNIX.py
@@ -7,33 +7,32 @@
 
 from chiplotle import *
 from chiplotle.utils.interactive_open_serial import interactive_open_serial
-from chiplotle.cfg.get_serial_port import get_serial_port
-from chiplotle.cfg.verify_config_file import verify_config_file
+from chiplotle.utils.interactive_choose_plotter import interactive_choose_plotter
+from chiplotle.cfg.read_config_value import read_config_value
+#from chiplotle.cfg.get_serial_port import get_serial_port
+#from chiplotle.cfg.verify_config_file import verify_config_file
 import serial
 
 print "\n* * * CHIPLOTLE in the house! * * *\n"
 
-verify_config_file( )
-port = get_serial_port( )
+#verify_config_file( )
+#port = get_serial_port( )
+## serial port
+port = read_config_value('serial_port')
 if port:
    ser = serial.Serial(port, 9600, timeout=.1)
 else:
    ser = interactive_open_serial( )
-#print ser
 
-### TODO: go to 'offline' mode if plotter is not found?
-plotter = plotters.Plotter(ser)
-print "\nPlotter with ID %s found in selected port." % plotter.id
+## plotter
+plt_name = read_config_value('plotter_type')
+if plt_name:
+   plotter = getattr(plotters, plt_name)(ser)
+else:
+   plotter = interactive_choose_plotter(ser)
 
-print "\nChoose a plotter type:"
-for i, plotter in enumerate(dir(plotters)):
-   print '[%d] %s' % (i,  plotter)
-
-plotter = plotters.__dict__[ dir(plotters)[int(raw_input())] ](ser)
-
-print "\nPlotter [plotter] with ID %s opened. " % plotter.id
+print "\nPlotter 'plotter' with ID %s opened. " % plotter.id
 print "Drawing area: %s" % plotter.marginSoft
-print "Status: %s" % plotter.status
 print "Buffer Size: %s" % plotter.bufferSize
 print "\n"
 
