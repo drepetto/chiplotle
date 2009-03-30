d88e69b27d650cf56f9d02a056ca4d00d98221ac
douglas <douglas@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2009-03-30 04:00:33 +0000

    added typewriter.py to examples.

---
+++ trunk/chiplotle/examples/typewriter.py
@@ -0,0 +1,48 @@
+from chiplotle import *
+from chiplotle.utils.run_chiplotle_UNIX import plotter
+
+
+print("***************************")
+print("* CHIPLOTLE TYPEWRITER!!! *")
+print("***************************")
+
+
+print("enter a blank line for options.\n\r")
+
+pen_num = int(raw_input("which pen? "))
+
+set_size = raw_input("set font size (y/N)? ")
+
+if set_size == "y":
+	char_height = float(raw_input("font height (in cm)? "))
+	char_width = float(raw_input("font width (in cm)? "))
+	plotter.write(SI(char_width, char_height))
+
+plotter.selectPen(pen_num)
+
+
+finished = False
+
+while finished == False:
+	line = raw_input(">>>")
+	if len(line) == 0:
+		print("(enter): blank line")
+		print("p: select new pen")
+		print("s: set new font size")
+		print("q: quit")
+		response = raw_input("command: ")
+		if response == "p":
+			pen_num = int(raw_input("which pen? "))
+			plotter.selectPen(pen_num)
+		elif response == "s":
+			char_height = float(raw_input("font height (in cm)? "))
+			char_width = float(raw_input("font width (in cm)? "))
+			plotter.write(SI(char_width, char_height))
+		elif response == "q":
+			finished = True
+		else:
+			plotter.write(LB("\n\r"))
+	else:
+		plotter.write(LB(line + "\n\r"))
+	
+print("l8r.")
