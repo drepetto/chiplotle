62dcd0df1848fa5feb8a2dfe538fa4bc870cbccb
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2009-04-03 15:08:31 +0000

    Added new scr directory to house all scripts. Added plot-hpgl script to plot an hpgl text file directly to the plotter. e.g. $ plot-hpgl myfile.hpgl
    Encapsulated the retrieval of serial port and the instantiation of a plotter code found in run_chiplotle_UNIX.py and placed in utils/instantiate_plotter.py.

--- trunk/chiplotle/utils/run_chiplotle_UNIX.py
+++ trunk/chiplotle/utils/run_chiplotle_UNIX.py
@@ -1,26 +1,31 @@
 from chiplotle import *
-from chiplotle.utils.interactive_open_serial import interactive_open_serial
-from chiplotle.utils.interactive_choose_plotter import interactive_choose_plotter
-from chiplotle.cfg.read_config_value import read_config_value
-import serial
+from chiplotle.utils.instantiate_plotter import instantiate_plotter
 
-print "\n* * * CHIPLOTLE in the house! * * *\n"
+plotter = instantiate_plotter( )
 
-## serial port
-port = read_config_value('serial_port')
-if port:
-   ser = serial.Serial(port, 9600, timeout=.1)
-else:
-   ser = interactive_open_serial( )
-
-## plotter
-plt_name = read_config_value('plotter_type')
-if plt_name:
-   plotter = getattr(plotters, plt_name)(ser)
-else:
-   plotter = interactive_choose_plotter(ser)
-
-print "Plotter 'plotter' with ID %s opened. " % plotter.id
-print "Drawing area: %s" % plotter.marginSoft
-print "Buffer Size: %s" % plotter.bufferSize
-print "\n"
+### OLD CODE. DELETE... 
+#from chiplotle.utils.interactive_open_serial import interactive_open_serial
+#from chiplotle.utils.interactive_choose_plotter import interactive_choose_plotter
+#from chiplotle.cfg.read_config_value import read_config_value
+#import serial
+#
+#print "\n* * * CHIPLOTLE in the house! * * *\n"
+#
+### serial port
+#port = read_config_value('serial_port')
+#if port:
+#   ser = serial.Serial(port, 9600, timeout=.1)
+#else:
+#   ser = interactive_open_serial( )
+#
+### plotter
+#plt_name = read_config_value('plotter_type')
+#if plt_name:
+#   plotter = getattr(plotters, plt_name)(ser)
+#else:
+#   plotter = interactive_choose_plotter(ser)
+#
+#print "Plotter 'plotter' with ID %s opened. " % plotter.id
+#print "Drawing area: %s" % plotter.marginSoft
+#print "Buffer Size: %s" % plotter.bufferSize
+#print "\n"
