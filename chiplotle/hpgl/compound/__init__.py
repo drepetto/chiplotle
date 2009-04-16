cf62096d7583c299fd29e01f7becf14ce515bc82
victor <vctradn@gmail.com>
2009-04-16 21:44:34 +0000

    hpgl/compound/__init__.py and plotters/__init__.py back to how they were. Need to figure out why Chiplotle is rejecting the new package import code.

--- chiplotle/hpgl/compound/__init__.py
+++ chiplotle/hpgl/compound/__init__.py
@@ -1,3 +1,8 @@
+#from chiplotle.utils.imports.package_import import _package_import
+#
+#_package_import(__path__[0], globals( ))
+
+
 from os import listdir
 from chiplotle.utils.import_cleaner import import_cleaner
 
