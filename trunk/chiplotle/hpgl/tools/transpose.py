2f827f6ec5cacafe2544f5e451e210f26b1bf139
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2009-04-20 21:46:54 +0000

    transpose(arg, val) tool function now raises a ValueError if the val attribute is not a pair.

--- trunk/chiplotle/hpgl/tools/transpose.py
+++ trunk/chiplotle/hpgl/tools/transpose.py
@@ -1,7 +1,10 @@
 from chiplotle.hpgl.abstract.hpglcommand import _HPGLCommand
 from chiplotle.hpgl.abstract.positional import _Positional
+from chiplotle.utils.ispair import ispair
 
 def _transpose_command(arg, val):
+   if not ispair(val):
+      raise ValueError('Transposing argument must be a pair (x, y).')
 #   if hasattr(arg, '_xy'):
    if isinstance(arg, _Positional) and arg._transposable:
       arg._xy[0::2] += val[0]
