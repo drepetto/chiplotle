c2855d55d23917a4ed1f295ed482856fdc7c1c8e
victor <vctradn@gmail.com>
2009-04-20 18:57:59 +0000

    Fixed bug in plotters initialization introduced in previous commits. Plotters were initializing prior to setting their allowedHPGLCommands list, giving an "unrecognized HPGL command" warning. Plotters should always set their allowedHPGLCommands list before calling their base class __init__.

--- chiplotle/plotters/hp7595a.py
+++ chiplotle/plotters/hp7595a.py
@@ -8,9 +8,6 @@ from chiplotle.plotters.drawingplotter import _DrawingPlotter
 
 class HP7595A(_DrawingPlotter):
    def __init__(self, ser, **kwargs):
-      _DrawingPlotter.__init__(self, ser, **kwargs)
-      self.type = "HP7595A"
-
       self.allowedHPGLCommands = tuple(['\x1b.', 'AA','AF','AH','AP','AR',
          'AS','BF','BL','CA','CC','CI','CM','CP','CS','CT','CV','DC','DF',
          'DI','DL','DP','DR','DS','DT','EA','EC','EP','ER','ES','EW','FP',
@@ -19,6 +16,9 @@ class HP7595A(_DrawingPlotter):
          'OK','OL','OO','OP','OS','OT','OW','PA','PB','PD','PG','PM','PR',
          'PT','PU','RA','RL','RO','RP','RR','SA','SC','SG','SI','SL','SM',
          'SP','SR','SS','TL','UC','UF','VA','VN','VS','WD','WG','XT','YT'])
+      _DrawingPlotter.__init__(self, ser, **kwargs)
+      self.type = "HP7595A"
+
 
 #      self.allowedHPGL2Commands = tuple(['\x1b.','AA','AC','AD','AR','AT',
 #         'BJ','BP','CF','CI','CP','CT','DC','DF','DI','DL','DP','DR','DT',
--- chiplotle/plotters/plotter.py
+++ chiplotle/plotters/plotter.py
@@ -2,11 +2,13 @@ from chiplotle.plotters.drawingplotter import _DrawingPlotter
 
 class Plotter(_DrawingPlotter):
    def __init__(self, ser, **kwargs):
-      _DrawingPlotter.__init__(self, ser, **kwargs)
-      self.type = "Generic"
-
+      ## allowedHPGLCommands must be set prior to base class init.
       self.allowedHPGLCommands = tuple(['(', ')','AA','AR','CA','CI','CP',
          'CS','DC','DF','DI','DP','DR','DT','EA','ER','EW','FT','IM','IN',
          'IP','IW','LB','LT','OA','OC','OD','OE','OF','OH','OI','OO','OP',
          'OS','OW','PA','PD','PR','PS','PT','PU','RA','RO','RR','SA','SC',
          'SI','SL','SM','SP', 'SR','SS','TL','UC','VS','WG','XT','YT'])
+
+      _DrawingPlotter.__init__(self, ser, **kwargs)
+      self.type = "Generic"
+
