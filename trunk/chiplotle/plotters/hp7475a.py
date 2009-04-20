9cf572d881a5f2b0d1c147ceb933dafd7e7782fb
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2009-04-20 05:28:17 +0000

    Fixed command validity checking in _BasePlotter. All commands are now checked for validity before being sent to the physical plotter. The code currently assumes that the ';' is *always* present as a command separator. Something may have broken. Tests pending.

--- trunk/chiplotle/plotters/hp7475a.py
+++ trunk/chiplotle/plotters/hp7475a.py
@@ -7,11 +7,13 @@
 from chiplotle.plotters.drawingplotter import _DrawingPlotter
 
 class HP7475A(_DrawingPlotter):
-    def __init__(self, ser, **kwargs):
-        _DrawingPlotter.__init__(self, ser, **kwargs)
-        self.type = "HP7475A"
+   def __init__(self, ser, **kwargs):
+      _DrawingPlotter.__init__(self, ser, **kwargs)
+      self.type = "HP7475A"
 
-        self.allowedHPGLCommands = tuple(['\x1b.', 'AA','AR','CA','CI','CP','CS','DC','DF','DI','DP','DR','DT','EA','ER','EW','FT','IM','IN',
-	'IP','IW','LB','LT','OA','OC','OD','OE','OF','OH','OI','OO','OP','OS','OW','PA','PD','PR','PS','PT','PU','RA',
-	'RO','RR','SA','SC','SI','SL','SM','SP','SR','SS','TL','UC','VS','WG','XT','YT'])
+      self.allowedHPGLCommands = tuple(['\x1b.', 'AA','AR','CA','CI','CP',
+         'CS','DC','DF','DI','DP','DR','DT','EA','ER','EW','FT','IM','IN',
+	      'IP','IW','LB','LT','OA','OC','OD','OE','OF','OH','OI','OO','OP',
+         'OS','OW','PA','PD','PR','PS','PT','PU','RA','RO','RR','SA','SC',
+         'SI','SL','SM','SP','SR','SS','TL','UC','VS','WG','XT','YT'])
 
--- trunk/chiplotle/plotters/plotter.py
+++ trunk/chiplotle/plotters/plotter.py
@@ -1,7 +1,12 @@
 from chiplotle.plotters.drawingplotter import _DrawingPlotter
 
 class Plotter(_DrawingPlotter):
-    def __init__(self, ser, **kwargs):
-        _DrawingPlotter.__init__(self, ser, **kwargs)
-        self.type = "Generic"
+   def __init__(self, ser, **kwargs):
+      _DrawingPlotter.__init__(self, ser, **kwargs)
+      self.type = "Generic"
 
+      self.allowedHPGLCommands = tuple(['(', ')','AA','AR','CA','CI','CP',
+         'CS','DC','DF','DI','DP','DR','DT','EA','ER','EW','FT','IM','IN',
+         'IP','IW','LB','LT','OA','OC','OD','OE','OF','OH','OI','OO','OP',
+         'OS','OW','PA','PD','PR','PS','PT','PU','RA','RO','RR','SA','SC',
+         'SI','SL','SM','SP', 'SR','SS','TL','UC','VS','WG','XT','YT'])
