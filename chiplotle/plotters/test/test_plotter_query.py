cdd9e964b51e7ab1c61d0f535e7b90e65bec47af
victor <vctradn@gmail.com>
2009-05-12 22:46:14 +0000

    Added plotting test file test_circle_plot.py.

--- chiplotle/plotters/test/test_plotter_query.py
+++ chiplotle/plotters/test/test_plotter_query.py
@@ -1,23 +1,20 @@
 from chiplotle import *
 from chiplotle.cfg.read_config_value import read_config_value
+from chiplotle.utils.instantiate_plotter import instantiate_plotter
 from serial import Serial
 
 def test_plotter_query_01( ):
-   '''
-   '''
-   port = read_config_value('serial_port')
-   if port:
-      s = Serial(port=port, baudrate=9600, timeout=0.1)
-      plotter = Plotter(s)
-      assert plotter._bufferSpace
-      assert plotter.id
-      assert plotter.actualPosition
-      assert plotter.commandedPosition
-      assert plotter.digitizedPoint
-      assert plotter.outputError
-      assert plotter.hardClipLimits
-      assert plotter.options
-      assert plotter.outputP1P2
-      assert plotter.status
-      assert plotter.window
+   plotter = instantiate_plotter( )
+
+   assert plotter._bufferSpace
+   assert plotter.id
+   assert plotter.actualPosition
+   assert plotter.commandedPosition
+   assert plotter.digitizedPoint
+   assert plotter.outputError
+   assert plotter.hardClipLimits
+   assert plotter.options
+   assert plotter.outputP1P2
+   assert plotter.status
+   assert plotter.window
 
