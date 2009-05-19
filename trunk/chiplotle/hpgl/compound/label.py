01f1a22177a2905176303d776f76943ff6f649ae
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2009-05-19 03:06:41 +0000

    Reformatted the chiplotle.hpgl.compound.label.Label docstring for reST compliance.

--- trunk/chiplotle/hpgl/compound/label.py
+++ trunk/chiplotle/hpgl/compound/label.py
@@ -3,29 +3,45 @@ from chiplotle.hpgl.scalable import Scalable
 from chiplotle.hpgl.commands import PU, LB, PA, ES, LO, SL, DI, DV, SI
 
 class Label(_CompoundHPGL):
-   '''Text label.
+   '''
+   Text label.
+
    Settable properties:
-   xy:         coordinates pair of label location.
-   text:       the actual text to be printed.
-   charsize:   (w, h) pair defining the absolute character size.
-   direction:  the inclination / angle of the text. Should be a pair of values: 
-               run (direction on x axis), rise (direction on y axis).
-   charspace:  factor to set spacing between characters. Positive separates,
-               negatives bring together. 
-   linespace:  factor to set spacing between lines. Positive separates, 
-               negatives bring together.
-   origin:     location of label relative to pen's current location. Possible 
-               values:
-                     Left Inside Right
-               Above   3     6     9
-               Inside  2     5     8
-               Below   1     4     7
-               If 10 is added to the above-mentioned location number, positions 
-               (except 5) will be offset towards the center by 1/2 the character 
-               width and 1/2 the character height. 
-   slant:      slant of characters (italic). Possible values: [0-1). 0 is vertical, 
-               0.5 is 45 degs., ...
-   vertical:   boolean; print text from left to right (False) or top down (True).
+
+   xy: 2-tuple
+      Coordinates pair of label location.
+
+   text: string
+      The actual text to be printed.
+
+   charsize:  2-tuple
+      (w, h) pair defining the absolute character size.
+   
+   direction: 2-tuple
+      the inclination / angle of the text: run (direction on x axis), rise (direction on y axis).
+
+   charspace: float
+      factor to set spacing between characters. Positive separates, negatives bring together. 
+
+   linespace: float
+      Factor to set spacing between lines. Positive separates, negatives bring together.
+
+   origin: int
+      location of label relative to pen's current location. Possible values:
+
+               Left Inside Right
+         Above   3     6     9
+         Inside  2     5     8
+         Below   1     4     7
+         If 10 is added to the above-mentioned location number, positions 
+         (except 5) will be offset towards the center by 1/2 the character 
+         width and 1/2 the character height. 
+
+   slant: float
+      slant of characters (italic). Possible values: [0-1). 0 is vertical, 0.5 is 45 degs., ...
+
+   vertical: boolean 
+      Print text from left to right (False) or top down (True).
    '''
 
    def __init__(self, xy, text):
