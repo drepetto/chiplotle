ab44c5faa6096d2a419d5dc955aa96bf6aa940fe
victor <vctradn@gmail.com>
2009-06-08 21:24:24 +0000

    Added new is_plotter_alive(port, wait_time=5) utility function. Returns True if there's a powered-on plotter in given *port*, otherwise False.
    Added call to is_plotter_alive( ) to instantiate_plotter( ) utility function.
    
    Cleaned up plotting tests.

--- chiplotle/utils/instantiate_plotter.py
+++ chiplotle/utils/instantiate_plotter.py
@@ -2,14 +2,20 @@ from chiplotle.cfg.read_config_value import read_config_value
 from chiplotle.utils.interactive_open_serial import interactive_open_serial
 from chiplotle.utils.interactive_choose_plotter import \
    interactive_choose_plotter
+from chiplotle.utils.is_plotter_alive import is_plotter_alive
 from chiplotle import plotters
 import serial
 
-def instantiate_plotter(port=None):
+def instantiate_plotter(port=None, wait_time=10):
    '''Instantiate a Plotter object associated with a port `port`. 
    If port is not passed as an argument it will be read from the config
    file. If not found in the config file the user will be prompted with
-   an interactive menu.'''
+   an interactive menu.
+   
+   - *port* : ``str`` location of serial port. Usually something like
+      '/def/ttyS0'. 
+   - *wait_time* : ``int`` maximum wait time for plotter response. 
+   '''
    ## serial port
    if not port:
       port = read_config_value('serial_port')
@@ -19,6 +25,10 @@ def instantiate_plotter(port=None):
       ser = interactive_open_serial( )
 
    ## plotter
+   if not is_plotter_alive(ser, wait_time):
+      print '*** ERROR: no plotter found in %s or not powered on.' % ser.port
+      return None
+
    plt_name = read_config_value('plotter_type')
    if plt_name:
       plotter = getattr(plotters, plt_name)(ser)
