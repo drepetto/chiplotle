b8fb59298c3ffc78211baa547b8823cbcc739865
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2009-06-11 21:03:25 +0000

    _CompoundHPGL commands now handle Pen class attributes.

--- trunk/chiplotle/hpgl/compound/compound.py
+++ trunk/chiplotle/hpgl/compound/compound.py
@@ -1,12 +1,31 @@
 from chiplotle.hpgl.abstract.positional import _Positional
 from chiplotle.hpgl.commands import SP
+from chiplotle.hpgl.compound.pen import Pen
 from chiplotle.hpgl.scalable import Scalable
+import types
 
 class _CompoundHPGL(_Positional):
    def __init__(self, xy, pen=None):
       _Positional.__init__(self, xy, True) 
-      self.pen = pen
       self._parent = None
+      self.pen = pen
+
+
+   @apply
+   def pen( ):
+      def fget(self):
+         return self._pen
+
+      def fset(self, pen):
+         if isinstance(pen, int):
+            self._pen = Pen(pen)
+         elif isinstance(pen, (Pen, types.NoneType)):
+            self._pen = pen
+         else:
+            raise TypeError('pen must be a Pen( ) instance, int or None.')
+
+      return property(**locals( ))
+         
 
    @property
    def xyabsolute(self):
@@ -15,13 +34,16 @@ class _CompoundHPGL(_Positional):
       else:
          return self.xy
 
+
    @property
    def xabsolute(self):
       return self._getAbsCoord(0)
 
+
    @property
    def yabsolute(self):
       return self._getAbsCoord(1)
+
 
    def _getAbsCoord(self, n):
       result = self.xyabsolute[n::2]
@@ -38,9 +60,11 @@ class _CompoundHPGL(_Positional):
    @property
    def _subcommands(self):
       result = [ ]
-      if not self.pen is None:
-         result.append(SP(self.pen))
+      if self.pen:
+         #result.append(SP(self.pen))
+         result.append(self.pen)
       return result
+
 
    @property
    def format(self):
