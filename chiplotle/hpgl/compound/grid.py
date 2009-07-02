62b6b73b97af490069011e9cc7efc85a83a0a0bb
victor <vctradn@gmail.com>
2009-07-02 21:47:11 +0000

    Fixed bug in new Grid _CompoundHPGL command.

--- chiplotle/hpgl/compound/grid.py
+++ chiplotle/hpgl/compound/grid.py
@@ -4,14 +4,24 @@ from chiplotle.hpgl.commands import PU, PD, PA, PR
 from chiplotle.hpgl.scalable import Scalable
 
 class Grid(_CompoundHPGL):
-   '''Rectangular grid. '''
-   def __init__(self, xy, width, height, width_line_count, 
-      height_line_count, pen=None):
+   '''Rectangular grid. 
+
+   - `xy` : ``tuple``, (x, y) position.
+   - `width` : ``int`` or ``float``, width of the rectangle.
+   - `height` : ``int`` or ``float``, height of the rectangle.
+   - `width_divisions` : ``int``, number of horizontal equidistant partitions.
+   - `horizontal_divisions` : ``int``, number of vertical equidistant 
+      partitions.
+   - `pen` : ``int``, pen number.
+   
+   '''
+   def __init__(self, xy, width, height, height_divisions, 
+      width_divisions, pen=None):
       _CompoundHPGL.__init__(self, xy, pen = pen) 
       self.width = Scalable(width)
       self.height = Scalable(height)
-      self.widthLineCount = width_line_count
-      self.heightLineCount = height_line_count
+      self.heightDivisions = height_divisions
+      self.widthDivisions = width_divisions
       self.referencePoint = (0, 0) ## range: [0 to 1]
 
 
@@ -20,33 +30,27 @@ class Grid(_CompoundHPGL):
       ul_x = self.xabsolute - self.referencePoint[0] * self.width
       bl_x = ul_x
       ur_x = ul_x + self.width
-      br_x = ur_x
 
       ul_y = self.yabsolute + self.referencePoint[1] * self.height
       ur_y = ul_y
       bl_y = ul_y - self.height
-      br_y = bl_y
 
-      ul = (ul_x, ul_y)
-      bl = (bl_x, bl_y)
-      ur = (ur_x, ur_y)
-      br = (br_x, br_y)
 
       result = _CompoundHPGL._subcommands.fget(self)
       ## add horizontal lines
-      for i in range(self.heightLineCount):
-         step_y = self.height / self.heightLineCount * i
-         result.append( PU(( ))
-         result.append( PR((ul_x, ul_y - step_y))
-         result.append( PD(( ))
-         result.append( PR((ur_x, ur_y - step_y))
+      for i in range(self.widthDivisions + 1):
+         step_y = self.height / self.widthDivisions * i
+         result.append(PU( ))
+         result.append(PA((ul_x, ul_y - step_y)))
+         result.append(PD( ))
+         result.append(PA((ur_x, ur_y - step_y)))
       ## add vertical lines
-      for i in range(self.widthLineCount):
-         step_x = self.width / self.widthLineCount * i
-         result.append( PU(( ))
-         result.append( PR((ul_x + step_x, ul_y))
-         result.append( PD(( ))
-         result.append( PR((bl_x + step_x, bl_y))
+      for i in range(self.heightDivisions + 1):
+         step_x = self.width / self.heightDivisions * i
+         result.append(PU( ))
+         result.append(PA((ul_x + step_x, ul_y)))
+         result.append(PD( ))
+         result.append(PA((bl_x + step_x, bl_y)))
 
       result.append(PU( ))
       return result
