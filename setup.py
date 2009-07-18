0c4be117406a284a6b77888dbf411f4a4455f509
victor <vctradn@gmail.com>
2009-07-18 15:01:27 +0000

    Fixed import bug in setup.py script.

--- setup.py
+++ setup.py
@@ -4,16 +4,14 @@
 #from distutils.core import setup
 from ez_setup import use_setuptools
 use_setuptools( ) ### this must come before setup import
-from setuptools import setup, find_packages( )
+from setuptools import setup, find_packages
 
 setup(name = 'Chiplotle', 
       version = '0.1', 
-      description = 'Chiplotle is an HPGL \
-      (Hewlett-Packard Graphics Language) Python API.',
-      long_description = 'Chiplotle is an HPGL \
-      (Hewlett-Packard Graphics Language) Python API.',
+      description = 'Chiplotle is an HPGL (Hewlett-Packard Graphics Language) Python API.',
+      long_description = 'Chiplotle is an HPGL (Hewlett-Packard Graphics Language) Python API.',
       author = 'Víctor Adán and Douglas Repetto',
-      author_email = 'vga2102@columbia.edu',
+      author_email = 'contact@victoradan.net',
       url = 'http://music.columbia.edu/cmc/chiplotle',
       keywords = 'vector graphics hpgl plotter plot pen',
       license = 'GPL', 
