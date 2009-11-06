4d613f5b772296cb02979a3ab076bcbc3f185b22
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2009-11-06 20:18:03 +0000

    Fixed bug in compound-HPGL command `Label`. Label.charsize was misformatting when set to None (the default). Added test_label.py file.

---
+++ trunk/chiplotle/hpgl/test/test_label.py
@@ -0,0 +1,27 @@
+from chiplotle import *
+
+def test_label_01( ):
+
+   t = Label((0, 0), 'Hello')
+
+   assert t.xy.tolist( ) == [0, 0]
+   assert t.text == 'Hello'
+   assert t.charsize == None
+   assert t.charsize == None
+   assert t.direction == None
+   assert t.charspace == None
+   assert t.linespace == None
+   assert t.origin == None
+   assert t.slant == None
+   assert t.vertical == False
+   assert t.format == 'PU;PA0.0,0.0;LBHello\x03;'
+
+
+def test_label_charsize_01( ):
+
+   t = Label((1, 1), 'Hello')
+   t.charsize = (2, 3)
+
+   assert isinstance(t.charsize, Scalable)
+   assert t.charsize.tolist( ) == [2, 3]
+   assert t.format == 'PU;PA1.0,1.0;SI2.00,3.00;LBHello\x03;SI;'
