249db6ef4d6d9e627a5147dadc70cff59795ff55
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2009-12-07 17:06:01 +0000

    New chiplotle.tools package to house all the user tools.
    Moved the "tools" subpackage in the "hpgl" package to the new chiplotle.tools package and renamed it "hpgltools".

--- trunk/chiplotle/hpgl/compound/__init__.py
+++ trunk/chiplotle/hpgl/compound/__init__.py
@@ -1,18 +1,24 @@
-#from chiplotle.utils.imports.package_import import _package_import
+from chiplotle.utils.imports.package_import import _package_import
+
+_package_import(__path__[0], globals( ))
+
+#from chiplotle.utils.imports.import_functions_in_package_to_namespace import _import_functions_in_package_to_namespace
+
+#_import_functions_in_package_to_namespace(__path__[0], globals( ))
+
+
+
+## TODO: delete this. The above call replaces this.
+#from os import listdir
+#from chiplotle.utils.import_cleaner import import_cleaner
 #
-#_package_import(__path__[0], globals( ))
-
-
-from os import listdir
-from chiplotle.utils.import_cleaner import import_cleaner
-
-_fns = listdir(__path__[0])
-_modules = [_fn[:-3] for _fn in _fns
-   if _fn.endswith('.py') and not _fn.startswith('_')]
-
-for _module in sorted(_modules):
-   exec('from %s import *' % _module)
-
-import_cleaner(locals())
-del import_cleaner
+#_fns = listdir(__path__[0])
+#_modules = [_fn[:-3] for _fn in _fns
+#   if _fn.endswith('.py') and not _fn.startswith('_')]
+#
+#for _module in sorted(_modules):
+#   exec('from %s import *' % _module)
+#
+#import_cleaner(locals())
+#del import_cleaner
 
