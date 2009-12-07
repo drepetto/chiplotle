5c4f4832c42d89d12c185b1b6b2316eac3169f21
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2009-12-07 23:34:28 +0000

    Refined descriptive string for PDF viewer in config.py file writer.

--- trunk/chiplotle/cfg/write_config_file.py
+++ trunk/chiplotle/cfg/write_config_file.py
@@ -33,7 +33,9 @@ def write_config_file(path):
    f.write("\n\n")
 
    f.write("# PDF viewer. Set for previewing HPGL commands via the\n")
-   f.write("# ``view( )`` function.\n")
+   f.write("# ``view( )`` function. If set to `None`, the viewer will\n")
+   f.write("# use the OS dependent generic file opener.\n")
+   f.write("# e.g., `open` in OS X, `xdg-open` in Linux.\n")
    f.write("pdf_viewer = None")
    f.write('\n\n')
 
