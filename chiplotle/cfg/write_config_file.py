09b1719493fd963e930520eb8bc65be18489615d
victor <vctradn@gmail.com>
2009-12-08 00:11:10 +0000

    Fixed bug in write_config_file( ). The function was incorrectly placing None values between quotes.

--- chiplotle/cfg/write_config_file.py
+++ chiplotle/cfg/write_config_file.py
@@ -25,11 +25,17 @@ def write_config_file(path):
    f.write("# Configuration Variables ---------------------------------\n\n")
 
    f.write("# Serial port to use for all connections.\n")
-   f.write("serial_port = '%s'" % serial_port)
+   if serial_port:
+      f.write("serial_port = '%s'" % serial_port)
+   else:
+      f.write("serial_port = None")
    f.write("\n\n")
 
    f.write("# Plotter type.\n")
-   f.write("plotter_type = '%s'" % plotter_type)
+   if plotter_type:
+      f.write("plotter_type = '%s'" % plotter_type)
+   else:
+      f.write("plotter_type = None")
    f.write("\n\n")
 
    f.write("# PDF viewer. Set for previewing HPGL commands via the\n")
@@ -54,7 +60,10 @@ def write_config_file(path):
 def _ask_serial_port( ):
    print "You can set a default serial port for Chiplotle to use every time it is run in live scripting mode. In POSIX type operating systems these ports are under the /dev directory. Serial ports usually look like ttyS0, ttyS1, etc. If you have a computer with no serial port and you are using a serial to USB converter then these ports typically look like ttyUSB0, ttyUSB1, etc. If you know what port your plotter is connected to you can set it as a default now so you don't have to tell Chiplotle what port to use every time you run it. If you don't know what port to use you can skip this setting and Chiplotle will ask you what port to use every time you run it live. You can always modify your default settings by editing the $HOME/.chiplotle/config.py file."
    sp = raw_input('Enter the FULL PATH to your serial port or hit Enter for default [None]:')
-   return sp
+   if sp == '':
+      return None
+   else:
+      return sp
 
 
 def _ask_plotter_type( ):
@@ -64,5 +73,8 @@ def _ask_plotter_type( ):
    message = '\nSet the default plotter type. Plotter types available are:\n%s\n' % plotter_list
    print message
    answer = raw_input('Enter the plotter number to set as default or hit Enter [None]:\n')
-   plt_name = dir(plotters)[int(answer)-1]
-   return plt_name
+   if answer == '':
+      return None
+   else:
+      plt_name = dir(plotters)[int(answer)-1]
+      return plt_name
