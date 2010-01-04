02639114721b33022599e133830145c5a7898ef0
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2010-01-04 21:23:47 +0000

    Removed 'OH' from allowed commands in DXY880 plotter. The plotter seems not to support Hard margins.

--- trunk/chiplotle/plotters/dxy880.py
+++ trunk/chiplotle/plotters/dxy880.py
@@ -9,7 +9,7 @@ class DXY880(_DrawingPlotter):
    def __init__(self, ser, **kwargs):
       self.allowedHPGLCommands = tuple(['\x1b.', 'AA','AR','CA','CI','CP',
          'CS','DC','DF','DI','DP','DR','DT','EA','ER','EW','FT','IM','IN',
-         'IP','IW','LB','LT','OA','OC','OD','OE','OF','OH','OI','OO','OP',
+         'IP','IW','LB','LT','OA','OC','OD','OE','OF','OI','OO','OP',
          'OS','OW','PA','PU','PD','PR','PS','PT','RA','RO','RR','SA','SC',
          'SI','SL','SM','SP','SR','SS','TL','UC','VS','WG','XT','YT'])
       _DrawingPlotter.__init__(self, ser, **kwargs)
