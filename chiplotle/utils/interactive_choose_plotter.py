8a55f5af0bf06e54213b3e7e766107ef447da6c6
victor <vctradn@gmail.com>
2010-01-04 20:41:04 +0000

    The interactive plotter chooser function now massages that recovered plotter id in order to properly recognize the DXY-? plotters.

--- chiplotle/utils/interactive_choose_plotter.py
+++ chiplotle/utils/interactive_choose_plotter.py
@@ -4,21 +4,17 @@ def interactive_choose_plotter(serial_port):
    tmp_plotter = plotters.Plotter(serial_port)
    id = tmp_plotter.id
    print "Found plotter with ID: %s" % id
+   ## massage id...
+   id = id.replace('-', '')
    ## try to instantiate a plotter that matches found ID...
    for plt_str in dir(plotters):
       if id in plt_str:
          plotter = getattr(plotters, plt_str)(serial_port)
          print "Instantiated plotter %s" % plotter
          return plotter
-   #ack, we're not recognizing the DXY-1300 correctly!
-      elif id == "DXY-1300":
-         plotter = getattr(plotters, "DXY1300")(serial_port)
-         print "Instantiated plotter %s" % plotter
-         return plotter
    ## not found, choose manually...
    else:
       print "ATTENTION: Plotter %s not supported." % id
-
       print "Choose the plotter that best fits your hardware."
       print "If in doubt choose the generic 'Plotter'."
       for i, plotter in enumerate(dir(plotters)):
@@ -27,12 +23,3 @@ def interactive_choose_plotter(serial_port):
       plotter = getattr(plotters, plt_name)(serial_port)
       return plotter
    
-#   plotter = plotters.Plotter(serial_port)
-#   print "\nFound plotter ID: %s." % plotter.id
-#   print "\nChoose a plotter type:"
-#   for i, plotter in enumerate(dir(plotters)):
-#      print '[%d] %s' % (i,  plotter)
-#   #plotter = plotters.__dict__[ dir(plotters)[int(raw_input())] ](serial_port)
-#   plt_name = dir(plotters)[int(raw_input())]
-#   plotter = getattr(plotters, plt_name)(serial_port)
-#   return plotter
