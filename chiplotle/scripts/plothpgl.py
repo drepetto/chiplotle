4025ea8b6b6b9df645998381f0ac7f84619d768c
victor <vctradn@gmail.com>
2010-01-05 15:51:16 +0000

    Added comment to code explaining why we need to call serial.flush( ).

--- chiplotle/scripts/plothpgl.py
+++ chiplotle/scripts/plothpgl.py
@@ -5,27 +5,16 @@ import time
 
 def plot_hpgl(file):
    '''Send an HPGL file to the plotter found connected to the computer.'''
-#   f = open(file, 'r')
-#   data = f.read( )#.splitlines( )
-#   print data
-#   f.close( )
-#
-#   plotter = instantiate_plotter( )
-#   ##plotter.write(data)
-
    plotter = instantiate_plotter( )
    plotter.writeFile(file)
-   
-   plotter._serialPort.flush()
-   #wait for serioal
-   #time.sleep(5)
+   ## call flush( ) to wait till all data is written before exiting...
+   plotter._serialPort.flush( )
+
 
 if __name__ == '__main__':
-
    if len(sys.argv) < 2:
       print 'Must give HPGL file to plot.\nExample: $ plothpgl myfile.hpgl'
       sys.exit(2)
-
    file = sys.argv[1]
 
    plot_hpgl(file) 
