2aeee5a3e524fe110cf07265a2de6d3a1852dced
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2010-01-05 23:13:09 +0000

    Container _CompoundHPGL class no longer inherits from list. It now explicitly defines access methods and overrides.

---
+++ trunk/chiplotle/hpgl/compound/test/test_container_setitem.py
@@ -0,0 +1,33 @@
+from chiplotle import *
+from py.test import raises
+
+def test_container_setitem_01( ):
+   '''Items set must be _CompoundHPGL.'''
+   circles = [Circle((0, 0), 50), Circle((100, 0), 100)]
+   t = Container((0, 0), circles)
+   
+   assert raises(TypeError, 't[0] = 3')
+
+
+def test_container_setitem_02( ):
+   circles = [Circle((0, 0), 50), Circle((100, 0), 100)]
+   t = Container((0, 0), circles)
+   t[0] = Rectangle((0, 0), 100, 200)
+
+   assert isinstance(t[0], Rectangle)
+   assert isinstance(t[1], Circle)
+   assert t[1] is circles[1]
+
+   
+def test_container_setitem_03( ):
+   '''Slices can be set.'''
+   circles = [Circle((0, 0), 50), Circle((100, 0), 100)]
+   t = Container((0, 0), circles)
+   t[0:2] = [Rectangle((0, 0), 100, 200), Rectangle((100, 0), 40, 30)]
+
+   assert isinstance(t[0], Rectangle)
+   assert isinstance(t[1], Rectangle)
+   assert t[0].xy.tolist( ) == [0, 0]
+   assert t[1].xy.tolist( ) == [100, 0]
+   
+   
