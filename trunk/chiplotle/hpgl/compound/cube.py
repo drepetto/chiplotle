1d441965f6e8155e35655edb8451cdd1b9e4ca65
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2010-01-05 03:24:23 +0000

    New Cube compound command available. Cubes are like Rectangles, but they can be rotated in 3D. Tests pending.
    Added rotate3d( ) function to the `geometry` module.

---
+++ trunk/chiplotle/hpgl/compound/cube.py
@@ -0,0 +1,63 @@
+from chiplotle.hpgl.compound.compound import _CompoundHPGL
+from chiplotle.hpgl.commands import PU, PD, PA
+from chiplotle.hpgl.scalable import Scalable
+from chiplotle.utils.geometry import *
+
+class Cube(_CompoundHPGL):
+   '''A 3D Cube. Can be rotated on x, y, and z.'''
+   def __init__(self, xy, width, height, depth, rotation=(0,0,0), pen=None):
+      _CompoundHPGL.__init__(self, xy, pen) 
+      self.width = Scalable(width)
+      self.height = Scalable(height)
+      self.depth = Scalable(depth)
+      if not len(rotation) == 3:
+         raise ValueError('`roration` must be a triple (xr, yr, zr)')
+      self.rotation = rotation
+
+   @property
+   def _subcommands(self):
+      blh = (-self.width / 2., -self.height / 2., self.depth / 2)
+      brh = (self.width / 2., -self.height / 2., self.depth / 2)
+      tlh = (-self.width / 2., self.height / 2., self.depth / 2)
+      trh = (self.width / 2., self.height / 2., self.depth / 2)
+
+      bll = (-self.width / 2., -self.height / 2., -self.depth / 2)
+      brl = (self.width / 2., -self.height / 2., -self.depth / 2)
+      tll = (-self.width / 2., self.height / 2., -self.depth / 2)
+      trl = (self.width / 2., self.height / 2., -self.depth / 2)
+      ## rotate ...
+      blh = rotate3d(blh, self.rotation)[0:2]
+      brh = rotate3d(brh, self.rotation)[0:2]
+      tlh = rotate3d(tlh, self.rotation)[0:2]
+      trh = rotate3d(trh, self.rotation)[0:2]
+      bll = rotate3d(bll, self.rotation)[0:2]
+      brl = rotate3d(brl, self.rotation)[0:2]
+      tll = rotate3d(tll, self.rotation)[0:2]
+      trl = rotate3d(trl, self.rotation)[0:2]
+
+      result = _CompoundHPGL._subcommands.fget(self)
+      result.append(PA( ))
+      ## high square...
+      result.append(PU(self.xyabsolute + blh))
+      result.append(PD(self.xyabsolute + brh))
+      result.append(PD(self.xyabsolute + trh))
+      result.append(PD(self.xyabsolute + tlh))
+      result.append(PD(self.xyabsolute + blh))
+      ## low square...
+      result.append(PU(self.xyabsolute + bll))
+      result.append(PD(self.xyabsolute + brl))
+      result.append(PD(self.xyabsolute + trl))
+      result.append(PD(self.xyabsolute + tll))
+      result.append(PD(self.xyabsolute + bll))
+      ## connect squares...
+      result.append(PU(self.xyabsolute + blh))
+      result.append(PD(self.xyabsolute + bll))
+      result.append(PU(self.xyabsolute + brh))
+      result.append(PD(self.xyabsolute + brl))
+      result.append(PU(self.xyabsolute + tlh))
+      result.append(PD(self.xyabsolute + tll))
+      result.append(PU(self.xyabsolute + trh))
+      result.append(PD(self.xyabsolute + trl))
+      result.append(PU( ))
+      return result
+
--- trunk/chiplotle/utils/geometry.py
+++ trunk/chiplotle/utils/geometry.py
@@ -15,6 +15,34 @@ def polar2xy(r, A):
 
 
 def rotate2d(xy, angle):
+   '''2D rotation.
+
+   - `xy` is a list or tuple with (x, y) coordinates.
+   - `angle` is the angle or rotation in radians.
+   '''
    xn = xy[0] * math.cos(angle) + xy[1] * math.sin(angle) 
    yn = - xy[0] * math.sin(angle) + xy[1] * math.cos(angle) 
    return (xn, yn)
+
+
+def rotate3d(xyz, xyzrot):
+   '''3D rotation. 
+
+   - `xyz` is a triple (x, y, z) of coordinates.
+   - `xyzrot` is a triple (xr, yr, zr) of angles of roation.
+   '''
+   if not len(xyz) == 3:
+      raise ValueError('Coordinate tuple `xyz` is not a triple.')
+   if not len(xyzrot) == 3:
+      raise ValueError('Rotation tuple `xyzrot` is not a triple.')
+   x, y, z = xyz
+   ## z rotation..
+   x = x * math.cos(xyzrot[2]) + y * math.sin(xyzrot[2]) 
+   y = - x * math.sin(xyzrot[2]) + y * math.cos(xyzrot[2]) 
+   ## y rotation..
+   x = x * math.cos(xyzrot[1]) + z * math.sin(xyzrot[1]) 
+   z = - x * math.sin(xyzrot[1]) + z * math.cos(xyzrot[1]) 
+   ## x rotation..
+   y = y * math.cos(xyzrot[0]) + z * math.sin(xyzrot[0]) 
+   z = - y * math.sin(xyzrot[0]) + z * math.cos(xyzrot[0]) 
+   return (x, y, z)
