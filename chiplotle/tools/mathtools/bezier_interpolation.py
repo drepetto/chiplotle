ab96d07a7f62dd4dd9013956d19a5310bac858f6
francesco <francesco@hv-a.com>
2010-03-17 11:20:19 +0000

    added Rational Bezier curves to Bezier compound class, now you can provide weights for control points. implementation is still a bit messy with numpy stuff, but it seems to work. bezier command is now Bezier(control_points, xy=None, points_to_compute=None, weight=None). weight is a list of weights for control points, must be same length of control_Points list or it will raise an exception

--- chiplotle/tools/mathtools/bezier_interpolation.py
+++ chiplotle/tools/mathtools/bezier_interpolation.py
@@ -1,14 +1,17 @@
 from chiplotle.tools.mathtools.pascal_row import pascal_row
+import numpy
 
-def bezier_interpolation(control_points, points_to_compute):
+def bezier_interpolation(points, points_to_compute, w):
       '''Computes Bezier interpolations from given `control_points`.
       This uses the generalized formula for bezier curves:
       http://en.wikipedia.org/wiki/B%C3%A9zier_curve#Generalization
 
       - `control_points` : A list of (x, y) control points.
       - `points_to_compute`: An int of the number of points to compute.
+      - `w`: A list of weights for control points.
       '''
 
+      control_points = numpy.array(points)
       number_points = len(control_points)
       combinations = pascal_row(number_points-1)
       ts = [t / float(points_to_compute) for t in range(points_to_compute + 1)]
@@ -17,8 +20,11 @@ def bezier_interpolation(control_points, points_to_compute):
       for t in ts:
          tpowers = (t**i for i in range(number_points))
          upowers = reversed([(1-t)**i for i in range(number_points)])
-         coefs = [c*a*b for c,a,b in zip(combinations,tpowers,upowers)]
-         point = tuple(sum([coef*p for coef,p in zip(coefs,ps)]) \
-            for ps in zip(*control_points))
+         coefs = numpy.array([c*a*b for c,a,b in zip(combinations,tpowers,upowers)])
+         num = numpy.column_stack((coefs*w*numpy.column_stack(control_points))).sum(axis=0)
+         den = (coefs*w).sum(axis=0)
+         if den == 0:
+            den = 1
+         point = tuple(num/den)
          result.append(point)
       return result
