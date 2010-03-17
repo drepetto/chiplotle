f7c6850d87fa021f367107f8a4ebb217fffd1bbc
francesco <francesco@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2010-03-17 11:20:19 +0000

    added Rational Bezier curves to Bezier compound class, now you can provide weights for control points. implementation is still a bit messy with numpy stuff, but it seems to work. bezier command is now Bezier(control_points, xy=None, points_to_compute=None, weight=None). weight is a list of weights for control points, must be same length of control_Points list or it will raise an exception

--- trunk/chiplotle/hpgl/compound/bezier.py
+++ trunk/chiplotle/hpgl/compound/bezier.py
@@ -6,10 +6,17 @@ from chiplotle.tools.mathtools import bezier_interpolation
 
 
 class Bezier(_CompoundHPGL):
-   '''Bezier curve aproximation'''
-   def __init__(self, control_points, xy=None, points_to_compute=None):
+   '''Bezier curve interpolation'''
+   def __init__(self, control_points, xy=None, points_to_compute=None, weight=None):
       self.control_points = Scalable(control_points)
       self.points_to_compute = points_to_compute or 100
+      if weight:
+         if len(weight) == len(self.control_points):
+            self.w = weight
+         else:
+            raise("weight list must be same length as control_points")
+      else:
+         self.w = 1
       xy = xy or (0, 0)
       _CompoundHPGL.__init__(self, xy) 
 
@@ -18,7 +25,7 @@ class Bezier(_CompoundHPGL):
    def _subcommands(self):
       plot_points = bezier_interpolation(
          self.control_points, 
-         self.points_to_compute)
+         self.points_to_compute, self.w)
 
       result = _CompoundHPGL._subcommands.fget(self)
       result.append(PU( ))
