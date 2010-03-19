e237cdbce84a1ec3974281d5c88ee71c4f178c93
francesco <francesco@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2010-03-19 21:27:57 +0000

    renamed variables to be more descriptive. in Bezier class changed construction argument from 'draw_controls' to 'control_marks', values can be 'crosses' or 'lines' (singular form 'cross' and 'line' is ok as well)

--- trunk/chiplotle/tools/mathtools/bezier_interpolation.py
+++ trunk/chiplotle/tools/mathtools/bezier_interpolation.py
@@ -1,17 +1,17 @@
 from chiplotle.tools.mathtools.pascal_row import pascal_row
 import numpy
 
-def bezier_interpolation(points, points_to_compute, w):
+def bezier_interpolation(control_points, points_to_compute, weight):
       '''Computes Bezier interpolations from given `control_points`.
       This uses the generalized formula for bezier curves:
       http://en.wikipedia.org/wiki/B%C3%A9zier_curve#Generalization
 
       - `control_points` : A list of (x, y) control points.
       - `points_to_compute`: An int of the number of points to compute.
-      - `w`: A list of weights for control points.
+      - `weight`: A list of weights for control points.
       '''
 
-      control_points = numpy.array(points)
+      control_points = numpy.array(control_points)
       number_points = len(control_points)
       combinations = pascal_row(number_points-1)
       ts = [t / float(points_to_compute) for t in range(points_to_compute + 1)]
@@ -21,8 +21,8 @@ def bezier_interpolation(points, points_to_compute, w):
          tpowers = (t**i for i in range(number_points))
          upowers = reversed([(1-t)**i for i in range(number_points)])
          coefs = numpy.array([c*a*b for c,a,b in zip(combinations,tpowers,upowers)])
-         num = numpy.column_stack((coefs*w*numpy.column_stack(control_points))).sum(axis=0)
-         den = (coefs*w).sum(axis=0)
+         num = numpy.column_stack((coefs*weight*numpy.column_stack(control_points))).sum(axis=0)
+         den = (coefs*weight).sum(axis=0)
          if den == 0:
             den = 1
          point = tuple(num/den)
