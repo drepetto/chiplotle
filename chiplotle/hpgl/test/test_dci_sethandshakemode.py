c84dca680cd23b48351cd3a472d48852b5f0b295
victor <vctradn@gmail.com>
2010-04-05 16:34:03 +0000

    Added test file for SetHandshakeMode( ).

---
+++ chiplotle/hpgl/test/test_dci_sethandshakemode.py
@@ -0,0 +1,22 @@
+from chiplotle import *
+from py.test import raises
+
+def test_dci_sethandshakemode_01( ):
+   '''SetHandshakeMode can take no parameters.'''
+   t = SetHandshakeMode( )
+
+   assert t.format == '\x1b.P'
+
+
+def test_dci_sethandshakemode_02( ):
+   '''SetHandshakeMode can take None, 0, 1, 2 or 3 to set the mode.'''
+   t = SetHandshakeMode(1)
+
+   assert t.format == '\x1b.P1'
+
+
+def test_dci_sethandshakemode_03( ):
+   '''SetHandshakeMode cannot take a value other than None, 0, 1, 2 or 3.'''
+   assert raises(ValueError, 't = SetHandshakeMode(32)')
+
+
