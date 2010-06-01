5e0f5d0fda9557da6c7b702947e54365f7d5d4df
victor <vctradn@gmail.com>
2010-06-01 05:04:18 +0000

    Changed camelCase to underscore_delimited in examples/mayanumbers.py

--- chiplotle/examples/mayanumbers.py
+++ chiplotle/examples/mayanumbers.py
@@ -1,8 +1,8 @@
 from chiplotle import *
 
 def main():
-   numberSize = 350
-   lineSpacing = numberSize * 3.1
+   number_size = 350
+   line_spacing = number_size * 3.1
 
    plotter = instantiate_plotters( )[0]
    if not plotter:
@@ -11,14 +11,14 @@ def main():
    width = plotter.margins.soft.width
    height = plotter.margins.soft.height
    left = plotter.margins.soft.left 
-   top = plotter.margins.soft.top - lineSpacing
+   top = plotter.margins.soft.top - line_spacing
 
-   numbersPerLine = width // 350
+   numbers_per_line = width // 350
    r = [ ]
    for i in range(0, 401, 1):
-      y = top - (i // numbersPerLine) * lineSpacing
-      x = left + (i % numbersPerLine) * numberSize
-      m = MayaNumber((x, y), i, numberSize)
+      y = top - (i // numbers_per_line) * line_spacing
+      x = left + (i % numbers_per_line) * number_size
+      m = MayaNumber((x, y), i, number_size)
       m.filled = False
       r.append(m)
    plotter.write(r)
