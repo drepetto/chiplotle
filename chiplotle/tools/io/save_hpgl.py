a80f360b7d2bd631724bbb9bd23a062c41db5702
douglas <douglas@music.columbia.edu>
2010-06-02 21:18:55 +0000

    save_hpgl.py no longer appends .hpgl to filename. I think this code should be renamed to save_hpgl_file.py to match import_hpgl_file.py and etc., but that might break things.

--- chiplotle/tools/io/save_hpgl.py
+++ chiplotle/tools/io/save_hpgl.py
@@ -7,7 +7,7 @@ def save_hpgl(expr, filename):
    '''Save text HPGL from Chiplotle-HPGL.
 
    - `expr` can be an iterable (e.g., list) or a Chiplotle-HPGL object.
-   - `filename` the file name, including path but without extension.
+   - `filename` the file name, including path and extension (usually .hpgl or .plt)
    '''
    ## parse expr; extract pure text HPGL commands.
    hpgl = [ ]
@@ -20,6 +20,6 @@ def save_hpgl(expr, filename):
       hpgl.append(expr.format)
 
    ## create HPGL file
-   file = open('%s.hpgl' % filename, 'w')
+   file = open('%s' % filename, 'w')
    file.writelines(hpgl)
    file.close( )
