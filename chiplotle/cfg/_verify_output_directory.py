23a85a52959647c9e75b4365171727a8892702ab
douglas <douglas@music.columbia.edu>
2010-07-06 21:50:50 +0000

    Changed verify_config routines to say Press enter instead of press any key. Any key doesn't work on OSX.

--- chiplotle/cfg/_verify_output_directory.py
+++ chiplotle/cfg/_verify_output_directory.py
@@ -6,5 +6,5 @@ def _verify_output_directory(directory = None):
    if not os.path.isdir(directory):
       raw_input('Attention: "%s" does not exist in your system.\n\
       Chiplotle will now create it to store all generated output files. \n\
-      Press any key to continue.' % directory)
+      Press enter to continue.' % directory)
       os.makedirs(directory)
