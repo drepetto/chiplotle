7d6c9d9fcc83e8a05e12177dbf45691deb4af8c1
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2010-09-24 20:17:34 +0000

    Added new configuration method `get_config_value( )` to read the values in the config file and display a clear error message if a variable is not found --usually due to a change in Chiplotle's config.py file design. The message prompts the user to delete the config file and rerun Chiplotle.

---
+++ trunk/chiplotle/cfg/get_config_value.py
@@ -0,0 +1,18 @@
+from chiplotle.cfg.read_config_file import read_config_file
+
+def get_config_value(var_name):
+   try:
+      result = read_config_file( )[var_name]
+      return result
+   except KeyError:
+      msg = ""
+      msg +="\n* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\a\n"
+      msg +="The configuration variable '%s'\n " % var_name
+      msg +="could not be found in the Chiplotle configuration file. \n"
+      msg +="This could be due to an update in Chiplotle that modified\n"
+      msg +="the configuration parameters of the file.\n" 
+      msg +="To fix,  please delete your configuration file and run\n"
+      msg +="Chiplotle again.\n"
+      msg +="* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\a\n"
+      raise ImportError(msg)
+      
