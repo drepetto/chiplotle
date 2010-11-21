13da63de0fbfe233ffa4d4e139eff77600f87d05
victor <vctradn@gmail.com>
2010-11-21 23:08:44 +0000

    mathtools.polar_to_xy( ) can now take a tuple pair (r, A). Added test file for mathtools.polar_to_xy( ).

--- chiplotle/tools/mathtools/polar_to_xy.py
+++ chiplotle/tools/mathtools/polar_to_xy.py
@@ -1,10 +1,15 @@
+from chiplotle.hpgl.coordinatepair import CoordinatePair
 import math
 
 
-def polar_to_xy(r, A):
-   '''Converts polar to Cartesian coordinates. 
-   Angle `A` is in radians.
+def polar_to_xy(*args):
+   '''Converts polar (r, A) to Cartesian (x y) coordinates,
+   where r is the radius and A is the angle in radians.
    '''
+   try:
+      r, A = tuple(CoordinatePair(args))
+   except:
+      raise TypeError('`args` must be an (r, A) tuple or a pair of values r, A.')
    x = r * math.cos(A)
    y = r * math.sin(A)
    return (x, y)
