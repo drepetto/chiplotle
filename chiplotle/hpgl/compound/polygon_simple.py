b98c782fa0e1c4514d74a408b0c8809bdaab68eb
douglas <douglas@music.columbia.edu>
2010-12-08 00:30:39 +0000

    Changed PolygonSimple behaviour: now checks to see if first_point == last_point and if not adds copy of first_point to [] of points.

--- chiplotle/hpgl/compound/polygon_simple.py
+++ chiplotle/hpgl/compound/polygon_simple.py
@@ -1,10 +1,12 @@
 from chiplotle.hpgl.compound.hpglcompoundshape import _HPGLCompoundShape
+from chiplotle.hpgl.coordinatepair import CoordinatePair
 from chiplotle.hpgl.commands import PU, PD, PA
 
 class PolygonSimple(_HPGLCompoundShape):
    '''A simple polygon, e.g. one that is simply a closed path.
       points is an [] of CoordinatePairs.
-      One final point (a duplicate of the first point) will be added to close the polygon.   
+      If first_point != last_point then one final point 
+      (a duplicate of the first point) will be added to close the polygon.   
    '''
 
    _scalable = _HPGLCompoundShape._scalable + ['width', 'height']
@@ -12,6 +14,14 @@ class PolygonSimple(_HPGLCompoundShape):
    def __init__(self, xy, points):
       _HPGLCompoundShape.__init__(self, xy)           
       self.points = points
+      
+      first_point = self.points[0]
+      last_point = self.points[len(points) - 1]
+      
+      if first_point.x != last_point.x or first_point.y != last_point.y:
+          new_last_point = CoordinatePair(first_point.x, first_point.y)
+          self.points.append(new_last_point)
+      
 
    @property
    def _subcommands(self):
@@ -23,8 +33,6 @@ class PolygonSimple(_HPGLCompoundShape):
       result.append( PD() )
       for point in self.points:
           result.append( PA((self.x + point.x, self.y + point.y)) )
-      #add final point to close the polygon
-      result.append( PA((self.x + first_point.x, self.y + first_point.y)) )
       result.append( PU() )
       return result
 
