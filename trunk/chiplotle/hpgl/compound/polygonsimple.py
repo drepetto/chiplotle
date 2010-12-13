13d6ebad25e947352348aac5763d929294285120
douglas <douglas@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2010-12-13 22:31:49 +0000

    Various updates to shapes.

--- trunk/chiplotle/hpgl/compound/polygonsimple.py
+++ trunk/chiplotle/hpgl/compound/polygonsimple.py
@@ -9,29 +9,33 @@ class PolygonSimple(_HPGLCompoundShape):
       (a duplicate of the first point) will be added to close the polygon.   
    '''
 
-   _scalable = _HPGLCompoundShape._scalable + ['width', 'height']
+   _scalable = _HPGLCompoundShape._scalable + ['width', 'height', 'points']
 
-   def __init__(self, xy, points):
+   def __init__(self, xy, poly_points):
       _HPGLCompoundShape.__init__(self, xy)           
-      self.points = points
+      self.poly_points = poly_points
       
-      first_point = self.points[0]
-      last_point = self.points[len(points) - 1]
+      first_point = self.poly_points[0]
+      last_point = self.poly_points[len(self.poly_points) - 1]
       
       if first_point != last_point:
           new_last_point = CoordinatePair(first_point)
-          self.points.append(new_last_point)
+          self.poly_points.append(new_last_point)
       
+   @property
+   def points(self):
+      return [self.poly_points]
 
    @property
    def _subcommands(self):
-      first_point = self.points[0]
+      the_points = self.points[0]
+      first_point = the_points[0]
       result = _HPGLCompoundShape._subcommands.fget(self)
       result.append( PU( ) )
       
       result.append( PA(self.xy + first_point) )
       result.append( PD() )
-      for point in self.points:
+      for point in the_points:
           result.append( PA(self.xy + point) )
       result.append( PU() )
       return result
