282b86a8142df258a1983787aaaeda8e908b00a6
victor <vctradn@gmail.com>
2010-12-14 19:30:51 +0000

    * Bug fix. Path in examples/arrow_decorator.py was being instantiated without the now mandatory `curvature` parameter.

--- chiplotle/examples/arrow_decorator.py
+++ chiplotle/examples/arrow_decorator.py
@@ -2,8 +2,9 @@ from chiplotle import *
 
 plotter = instantiate_plotters( )[0]
 
-line = Path([(0, 0), (1000, 1000), (-1000, 1000), (0, 2000)])
+line = Path([(0, 0), (1000, 1000), (-1000, 1000), (0, 2000)], 1)
 arrow = DArrow(line, 500, 1000)
 
-plotter.write(arrow)
+io.view(arrow)
+#plotter.write(arrow)
 
