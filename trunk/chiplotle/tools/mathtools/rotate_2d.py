34303b55a95cfa5a3e8f5825b75b48a37e9f52a4
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2010-12-15 16:11:28 +0000

    * mathtools.rotate_2d can no take a `pivot` point arond which to rotate. (0, 0) is the default rotation point.

--- trunk/chiplotle/tools/mathtools/rotate_2d.py
+++ trunk/chiplotle/tools/mathtools/rotate_2d.py
@@ -2,16 +2,22 @@ from chiplotle.hpgl.coordinatepair import CoordinatePair
 import math
 
 
-def rotate_2d(xy, angle):
+def rotate_2d(xy, angle, pivot=(0, 0)):
    '''2D rotation.
 
-   - `xy` is a list or tuple with (x, y) coordinates.
-   - `angle` is the angle or rotation in radians.
+   - `xy` is an (x, y) coordinate pair.
+   - `angle` is the angle of rotation in radians.
+   - `pivot` the point around which to rotate `xy`.
+
+   Returns a CoordinatePair.
    '''
-   xy = CoordinatePair(xy)
-   xn = xy[0] * math.cos(angle) + xy[1] * math.sin(angle) 
-   #yn = - xy[0] * math.sin(angle) + xy[1] * math.cos(angle) 
-   yn =  xy[0] * math.sin(angle) - xy[1] * math.cos(angle) 
-   return (xn, yn)
+   ## rotate counter-clockwise...
+   angle = -angle
+   cp = CoordinatePair(xy)
+   cp -= pivot
+   x = cp.x * math.cos(angle) + cp.y * math.sin(angle) 
+   y =  -cp.x * math.sin(angle) + cp.y * math.cos(angle) 
+   result = CoordinatePair(x, y) + pivot
+   return result
 
 
