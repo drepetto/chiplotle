c406fabbad5d00ba789c25de43115baba5c1709a
victor <vctradn@gmail.com>
2010-12-15 18:11:49 +0000

    * hpgltools.convert_coordinates_to_hpgl_absolute_path( ) can now take a CoordinateArray.

--- chiplotle/tools/hpgltools/convert_coordinates_to_hpgl_absolute_path.py
+++ chiplotle/tools/hpgltools/convert_coordinates_to_hpgl_absolute_path.py
@@ -1,12 +1,13 @@
 from chiplotle.hpgl.commands import PU, PA, PD
+from chiplotle.hpgl.coordinatearray import CoordinateArray
 
 def convert_coordinates_to_hpgl_absolute_path(coords):
    '''Converts an iterator of lists of coordinates
    e.g., [<x1, y1>, <x2, y2>, <x3, y3>, ...]
    into a list of PA, PD and PU HPGL commands. 
    '''
-   if not isinstance(coords, (list, tuple)):
-      raise TypeError('`coords` must be a list of coordinates.')
+   if not isinstance(coords, (list, tuple, CoordinateArray)):
+      raise TypeError('`coords` must be a list of coordinates or CoordinateArray.')
 
    result = [ ]
    result.append(PU( ))
--- chiplotle/tools/hpgltools/test/test_convert_coordinates_to_absolute_path.py
+++ chiplotle/tools/hpgltools/test/test_convert_coordinates_to_absolute_path.py
@@ -22,6 +22,16 @@ def test_convert_coordinates_to_absolute_hpgl_path_02( ):
 
 
 def test_convert_coordinates_to_absolute_hpgl_path_03( ):
+   '''The argument can be a CoordinateArray'''
+   c = CoordinateArray([CoordinatePair(1, 2), CoordinatePair(3, 4), CoordinatePair(5, 6)])
+   t = hpgltools.convert_coordinates_to_hpgl_absolute_path(c)
+   assert t[0] == PU( )
+   assert t[1] == PA(c[0])
+   assert t[2] == PD( )
+   assert t[3] == PA(c[1:])
+
+
+def test_convert_coordinates_to_absolute_hpgl_path_04( ):
    '''Argument must be a list of coordinate pairs..'''
    c = [1, 2, 3, 4]
    assert raises(TypeError, 'hpgltools.convert_coordinates_to_hpgl_absolute_path(c)')
