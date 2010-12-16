c9087309e555dcf85237d78091231b766db7bc4d
douglas <douglas@music.columbia.edu>
2010-12-16 02:56:50 +0000

    Removing unused imports from shapes.

--- chiplotle/shapes/polygon.py
+++ chiplotle/shapes/polygon.py
@@ -1,7 +1,6 @@
 from chiplotle.shapes.shape import _Shape
 from chiplotle.hpgl.coordinatearray import CoordinateArray
 from chiplotle.hpgl.coordinatepair import CoordinatePair
-from chiplotle.hpgl.commands import PU, PD, PA
 
 class Polygon(_Shape):
    '''
