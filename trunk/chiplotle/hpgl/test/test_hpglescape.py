10e3a4caef80b09888d213857840c40652f76ba5
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2010-12-17 17:26:45 +0000

    * The _HPGLEscape._escape class attribute is no longer settable from concrete instances. Removed call to the get_base_class( ) utility helper for deprecation and removal.

--- trunk/chiplotle/hpgl/test/test_hpglescape.py
+++ trunk/chiplotle/hpgl/test/test_hpglescape.py
@@ -1,13 +1,14 @@
 from chiplotle import *
 
-def test_hpglescape_escape_01( ):
-   '''The escape class attribute of _HPGLEscape commands can be set directly
-   from the instances.'''
-   on = On( )
-   off = Off( )
-   on.escape = chr(1)
-   assert on.escape == chr(1)
-   assert off.escape == chr(1)
-   off.escape = chr(2)
-   assert on.escape == chr(2)
-   assert off.escape == chr(2)
+## No longer the case
+#def test_hpglescape_escape_01( ):
+#   '''The escape class attribute of _HPGLEscape commands can be set directly
+#   from the instances.'''
+#   on = On( )
+#   off = Off( )
+#   on.escape = chr(1)
+#   assert on.escape == chr(1)
+#   assert off.escape == chr(1)
+#   off.escape = chr(2)
+#   assert on.escape == chr(2)
+#   assert off.escape == chr(2)
