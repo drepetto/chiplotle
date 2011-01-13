afe5aa518798114e09f84cf7cb27764629465302
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-01-13 13:20:46 +0000

    New geometry/destructive_transforms folder available. This folder will house transforms analogous to the geometry/transforms, and will probably replace this less recent geometry/transforms classes. In contrast with the geometry/transforms, these: 1. are 'destructive', in the sense that they immediately modify the shape they are given to modify. 2. they assume the geometry model is made of basically abstract paths.
    Currently two transforms exist: noise( ) and perpendicular_noise( ). The first simply displaces all points in the given shape randomly in the whole x,y plane. The second displaces randomly along the perpendicular of the given path. Run them both to see the difference.

---
+++ trunk/chiplotle/geometry/destructive_transforms/noise.py
@@ -0,0 +1,41 @@
+from chiplotle.geometry.shapes.group import Group
+from chiplotle.geometry.shapes.path import Path
+import random
+
+def noise(shape, value):
+   '''Distort shape by adding noise.
+
+   - `value` can be a scalar or a tuple (x, y) that sets the range of the 
+      noise for the x and y coordinates.
+   '''
+   if isinstance(shape, Group):
+      for i in range(len(shape)):
+         shape[i] = noise(shape[i], value)
+   else:
+      try:
+         x, y = value
+      except TypeError:
+         x = y = value
+      result = [ ]
+      for point in shape.points[0]:
+         x_wiggle = random.randrange(-x, x)
+         y_wiggle = random.randrange(-y, y)
+         xy = point + (x_wiggle, y_wiggle)         
+         result.append(xy)
+      shape.points = result
+
+   return shape
+      
+      
+   
+## RUN DEMO CODE
+if __name__ == "__main__":
+   from chiplotle.geometry.factory.circle import circle
+   from chiplotle.tools import io
+   c1 = circle(100)
+   c2 = circle(80)
+   cn = noise(c1, 6)
+   cn += 100
+   g = Group([cn, c2])
+   g = noise(g, 6)
+   io.view(g)
---
+++ trunk/chiplotle/geometry/destructive_transforms/perpendicular_noise.py
@@ -0,0 +1,43 @@
+from chiplotle.geometry.shapes.group import Group
+from chiplotle.geometry.shapes.path import Path
+from chiplotle.tools.mathtools.difference import difference
+import random
+
+def perpendicular_noise(shape, value):
+   '''Distort shape by adding noise perpendiculary to the path.
+   This is an in-place destructive transformation; no new shapes are created.
+
+   - `shape` is the shape to be noisified.
+   - `value` must be a scalar defining the range of the noise 
+      for displacement.
+   '''
+   if isinstance(shape, Group):
+      for i in range(len(shape)):
+         shape[i] = perpendicular_noise(shape[i], value)
+   else:
+      result = [ ]
+      points = shape.points[0]
+      d_points = difference(points)
+      for point, d_point in zip(points[:-1], d_points):
+         wiggle = random.randrange(-value, value)
+         perp = ~d_point 
+         xy = point + perp / perp.magnitude * wiggle         
+         result.append(xy)
+      shape.points = result
+
+   #return shape
+      
+      
+   
+## RUN DEMO CODE
+if __name__ == "__main__":
+   from chiplotle.geometry.factory.circle import circle
+   from chiplotle.tools import io
+   c1 = circle(100, 200)
+   c2 = circle(80, 200)
+   c2 += 200
+   perpendicular_noise(c1, 36)
+   perpendicular_noise(c2, 6)
+   g = Group([c1, c2])
+   io.view(g)
+
