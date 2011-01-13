c569bfdad246909647666e69ab4aa35e757f8b16
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-01-13 13:50:16 +0000

    rotate(shape, angle, pivot) function available under geometry/destructive_transforms. Again, are the 'offset' and 'rotation' attributes of _Shape less practical and more complex than these external transformations?

---
+++ trunk/chiplotle/geometry/destructive_transforms/rotate.py
@@ -0,0 +1,29 @@
+from chiplotle.tools.mathtools.rotate_2d import rotate_2d
+from chiplotle.geometry.shapes.group import Group
+
+def rotate(shape, angle, pivot = (0, 0)):
+   '''In place rotation.
+
+   - `shape` is the shape to be rotated.
+   - `angle` is the angle (in radians) of rotation.
+   - `pivot` is an coordinate tuple used as center of rotation; 
+      default is (0, 0).
+   '''
+
+   if isinstance(shape, Group):
+      for s in shape:
+         rotate(s, angle, pivot)
+   else:
+      shape.points = rotate_2d(shape.points[0], angle, pivot)
+
+
+
+## RUN DEMO CODE
+if __name__ == "__main__":
+   from chiplotle.geometry.factory.rectangle import rectangle
+   from chiplotle.tools import io
+   r1 = rectangle(100, 40)
+   r2 = rectangle(100, 40)
+   rotate(r1, 3.14 / 4)
+   rotate(r2, 3.14 / 4, (100, 100))
+   io.view(Group([r1, r2]))
