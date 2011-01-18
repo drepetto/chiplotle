e3c502dbebda7c5c22f904cc66f064da69b39d25
douglas <douglas@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-01-18 02:40:33 +0000

    Tweaked lcm.py to use fractions.gcd

--- trunk/chiplotle/tools/mathtools/lcm.py
+++ trunk/chiplotle/tools/mathtools/lcm.py
@@ -1,19 +1,19 @@
-from chiplotle.tools.mathtools.gcf import gcf
+from fractions import gcd
 
 def lcm(a, b):
     ''' returns the lowest common multiple of a & b'''
     
-    the_gcf = 0
+    the_gcd = 0
     lcm = 0
     
     if a > b:
         dividend = a
         divisor = b
     else:
-        dividend=b
-        divisor=a
+        dividend = b
+        divisor = a
         
-    the_gcf = gcf(dividend,divisor)
-    lcm = (a * b) / the_gcf
+    the_gcd = gcd(dividend,divisor)
+    lcm = (a * b) / the_gcd
     
     return lcm
