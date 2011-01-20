debbbd1f828a0d7a27714457d56de312b2caccaf
douglas <douglas@music.columbia.edu>
2011-01-20 04:16:35 +0000

    More tweaks to star_crisscross.py, added an option to bail out if jump_size is invalid, added nice demo in main().

--- chiplotle/geometry/factory/star_crisscross.py
+++ chiplotle/geometry/factory/star_crisscross.py
@@ -4,14 +4,13 @@ from chiplotle.tools.mathtools.lcm import lcm
 import math
 from fractions import gcd
 
-def star_crisscross(width, height, num_points = 5, jump_size = None):  
+def star_crisscross(width, height, num_points = 5, jump_size = None, find_valid_jump_size = True):  
    '''
       Draws a star with criscrossing lines.
       
       jump_size determines how many points to skip between connected points.
-      illegal jump sizes (ones that do not result in valid crisscross stars)
-      are ignored and replaced with valid ones. 
-   
+      an illegal jump size (one that does not result in a valid crisscross star)
+      is ignored and replaced with a dot in the center of the star.
    '''
    corners = []
    pi_div_180 = math.pi / 180.0
@@ -49,17 +48,18 @@ def star_crisscross(width, height, num_points = 5, jump_size = None):
    else:
       if jump_size is None:
          jump_size = int(num_points/2)
-         print jump_size
-      
-      while gcd(num_points, jump_size) != 1:
-         jump_size -= 1
-         print jump_size
-         
-      print jump_size
+
+      if gcd(num_points, jump_size) != 1:
+         if find_valid_jump_size:
+            while gcd(num_points, jump_size) != 1:
+               jump_size -= 1
+         else:
+            invalid_star = [[width/2, height/2], [width/2, height/2]]
+            return Path(invalid_star)
       
       point_order = []
       for i in range(0, num_points + 1):
-         point_num =  i * jump_size % num_points
+         point_num =  (i * jump_size) % num_points
          point_order.append(point_num)
       
       corners = [corners[i] for i in point_order]
@@ -70,24 +70,16 @@ def star_crisscross(width, height, num_points = 5, jump_size = None):
 ## RUN DEMO CODE
 if __name__ == '__main__':
    from chiplotle.tools import io
-   import math
+   from chiplotle.geometry.factory.star_crisscross import star_crisscross
+   from chiplotle.geometry.shapes.group import Group
+   from chiplotle.geometry.destructive_transforms.offset import offset
    
-   s1 = star_crisscross(1000, 1000)
-
-   ## displaced
-   s2 = star_crisscross(1000, 1000, num_points = 7)
-   s2.offset = (1000, 0)
-
-   ## displaced and rotated 
-   s3 = star_crisscross(1000, 1000)
-   s3.offset = (2000, 0)
-   s3.rotation = math.pi / 3.0
-
-   ## displaced and rotated around pivot
-   s4 = star_crisscross(1000, 1000, num_points = 9)
-   s4.offset = (3000, 0)
-   s4.rotation = math.pi / 3.0
-   s4.pivot = (100, 100)
-
-   gr = Group([s1, s2, s3, s4])
+   gr = Group()
+   
+   for points in range(5, 26):
+      for i in range(1, points):
+         s = star_crisscross(1000,1000,num_points = points, jump_size = i, find_valid_jump_size = False)
+         offset(s, ((i - 1) * 1000, -(points - 5) * 1000))
+         gr.append(s)
+   
    io.view(gr)
