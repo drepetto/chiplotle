53ffa9cc717c7682bb2318fe336a8fcaf2f00228
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-01-23 22:15:33 +0000

    the 'scale(shape, value, pivot)' destructive transform now takes a pivot to set the center of the scaling transformation.

--- trunk/chiplotle/geometry/destructive_transforms/scale.py
+++ trunk/chiplotle/geometry/destructive_transforms/scale.py
@@ -1,17 +1,34 @@
 from chiplotle.geometry.shapes.group import Group
+from chiplotle.geometry.coordinate import Coordinate
 
-def scale(shape, value):
+def scale(shape, value, pivot = Coordinate(0, 0)):
    '''In place scaling.
 
    - `shape` is the shape to be rotated.
    - `value` is the scaling value. Can be a scalar or an (x, y) coordinate.
+   - `pivot` is the Coordinate around which the shape will be scaled.
    '''
+
+   try: ## if it's a tuple, convert to Coordinate...
+      ## NOTE Coordinate is a poor name for this, Vector is a more general 
+      ## notion and thus more appropriate here.
+      value = Coordinate(*value)
+   except TypeError:
+      pass
+
+   pivot = Coordinate(*pivot)
 
    if isinstance(shape, Group):
       for s in shape:
-         scale(s, value)
+         scale(s, value, pivot)
    else: ## it's a Path...
-      shape.points = shape.points * value
+      ## TODO use matrices to make this more efficient? 
+      if pivot == Coordinate(0, 0):
+         shape.points = shape.points * value
+      else:
+         offset_points = shape.points - pivot
+         scaled_points = offset_points * value
+         shape.points = scaled_points + pivot
 
 
 
@@ -21,7 +38,7 @@ if __name__ == "__main__":
    from chiplotle.tools import io
    r1 = rectangle(100, 40)
    r2 = rectangle(100, 40)
-   scale(r1, 10)
+   scale(r1, 10, (50, 50))
    scale(r2, (10, 20))
    io.view(Group([r1, r2]))
 
