d42b56fc89258a21f9b25ebc99fd722ba2f95ffb
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-02-02 03:42:32 +0000

    Simplified / cleaned the code in geometrytools.get_bounding_coordinate_pairs( ).

--- trunk/chiplotle/tools/geometrytools/get_bounding_coordinate_pairs.py
+++ trunk/chiplotle/tools/geometrytools/get_bounding_coordinate_pairs.py
@@ -1,5 +1,6 @@
 from chiplotle.geometry.coordinate import Coordinate
-import sys
+from chiplotle.geometry.coordinatearray import CoordinateArray
+#import sys
 
 def get_bounding_coordinate_pairs(shape):
    '''Returns the pair of coordinate pairs outlining the bounding box of
@@ -7,25 +8,36 @@ def get_bounding_coordinate_pairs(shape):
    
    ## TODO add type checking?
 
-   min_x = min_y = sys.maxint
-   max_x = max_y = -sys.maxint
-   
-   coords = shape.points
+   coords = CoordinateArray(shape.points)
 
    if len(coords) == 0:
       return None
 
-   for c in coords:
-      ## x...
-      if c.x > max_x:
-         max_x = c.x
-      if c.x < min_x:
-         min_x = c.x
-      ## y...
-      if c.y > max_y:
-         max_y = c.y
-      if c.y < min_y:
-         min_y = c.y
+   max_x = max(coords.x)
+   min_x = min(coords.x)
+   max_y = max(coords.y)
+   min_y = min(coords.y)
+   
+
+#   min_x = min_y = sys.maxint
+#   max_x = max_y = -sys.maxint
+#   
+#   coords = shape.points
+#
+#   if len(coords) == 0:
+#      return None
+#
+#   for c in coords:
+#      ## x...
+#      if c.x > max_x:
+#         max_x = c.x
+#      if c.x < min_x:
+#         min_x = c.x
+#      ## y...
+#      if c.y > max_y:
+#         max_y = c.y
+#      if c.y < min_y:
+#         min_y = c.y
 
    return (Coordinate(min_x, min_y), Coordinate(max_x, max_y))
 
