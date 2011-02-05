fcd9fc0d1677b69cb15dabf7ee3402ebaf1f8847
douglas <douglas@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-02-05 04:09:52 +0000

    Put random_walk_polar() and random_walk_cartesian() in separate files.

--- trunk/chiplotle/geometry/factory/random_walk_polar.py
+++ trunk/chiplotle/geometry/factory/random_walk_polar.py
@@ -19,33 +19,13 @@ def random_walk_polar(steps, step_size=500):
    return Path(the_points)
 
 
-def random_walk_cartesian(steps, step_size=500):
-   '''Random Walk. Border is a square.'''
-
-   the_points = []
-   half_step = step_size/2
-   
-   previous = (0,0)
-   
-   for i in range(steps):
-      x = previous[0] + random.randrange(-half_step, half_step)
-      y = previous[1] + random.randrange(-half_step, half_step)      
-      the_points.append((x, y))
-   
-   return Path(the_points)
-
-
-
 ## RUN DEMO CODE
 
 if __name__ == '__main__':
    from chiplotle.tools import io
+   
    rw = random_walk_polar(1000)
    assert isinstance(rw, Path)
-   print rw.format
+   #print rw.format
    io.view(rw)
 
-   rw = random_walk_cartesian(1000)
-   assert isinstance(rw, Path)
-   print rw.format
-   io.view(rw)
