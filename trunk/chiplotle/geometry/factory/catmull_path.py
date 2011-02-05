d99620eec6c80e8af1a61669c14544ed8ce41d94
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-02-05 04:24:40 +0000

    New 'catmull_path(point, interpolation_count)' shape-maker available.

---
+++ trunk/chiplotle/geometry/factory/catmull_path.py
@@ -0,0 +1,37 @@
+from chiplotle.geometry.shapes.path import Path
+from chiplotle.tools.mathtools.catmull_interpolation \
+   import catmull_interpolation
+
+
+def catmull_path(points, interpolation_count=50):
+   '''Path with Catmull-Rom spline interpolation.'''
+
+   path_points = catmull_interpolation(points, interpolation_count)
+   return Path(path_points)
+
+
+
+## DEMO
+if __name__ == '__main__':
+   from chiplotle.tools import io
+   from chiplotle.geometry.factory.cross import cross
+   from chiplotle.geometry.shapes.group import Group
+   from chiplotle.geometry.destructive_transforms.offset import offset
+   import random
+
+   points = [ ]
+   for i in range(10):
+      x, y = random.randint(-100, 100), random.randint(-100, 100)
+      points.append((x, y))
+
+   crosses = [ ]
+   for point in points:
+      c = cross(15, 15)
+      offset(c, point)
+      crosses.append(c)
+
+   path = catmull_path(points)
+   
+   g = Group([path] + crosses)
+   io.view(g)
+
