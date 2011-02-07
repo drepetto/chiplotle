2eea986392a0ae60e16c9e4ff7c861c5699b2934
douglas <douglas@music.columbia.edu>
2011-02-07 04:47:39 +0000

    Tweaked arch spiral.

--- chiplotle/geometry/factory/archimedean_spiral.py
+++ chiplotle/geometry/factory/archimedean_spiral.py
@@ -1,15 +1,16 @@
 from chiplotle.geometry.shapes.path import Path
 import math
 
-def archimedean_spiral(num_turns = 5, turn_spacing = 100, direction = "cw", segments = 500):  
+def archimedean_spiral(num_turns = 5, turn_spacing = 100, direction = "cw", exponent = 1.0, segments = 500):  
    '''
    Constructs an Archimedean (arithmetic) spiral with the given number of
-   turns, turn spacing, direction ("cw" or "ccw"), and number of segments.
+   turns, turn spacing, direction ("cw" or "ccw"), exponent, and number of
+   segments.
    
    The default values will create a CW spiral with a right half that is
    500 (num_turns * turn_spacing) units from the center to the edge. 
    
-   r = b * theta
+   r = b * theta^1/x
    
    '''
 
@@ -26,8 +27,8 @@ def archimedean_spiral(num_turns = 5, turn_spacing = 100, direction = "cw", segm
    spiral_points = []
    
    for i in range(segments):
-      r_width = radius_multi * theta 
-      r_height = radius_multi * theta
+      r_width = radius_multi * (pow(theta, exponent)) 
+      r_height = radius_multi * (pow(theta, exponent))
       x = math.cos(theta) * r_width;
       y = math.sin(theta) * r_height;
       spiral_points.append((x, y))
@@ -47,7 +48,7 @@ if __name__ == '__main__':
    from chiplotle.geometry.factory.line import line
    from chiplotle.tools import io
    
-   s = archimedean_spiral()
+   s = archimedean_spiral(exponent = .2)
    assert isinstance(s, Path)
  
    line_right = line((0,0), (500, 0))
