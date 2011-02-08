e4226693a975a64521c8bf7cefe0941b5e669168
douglas <douglas@music.columbia.edu>
2011-02-08 04:09:41 +0000

    Simplified archimedean_spiral.py

--- chiplotle/geometry/factory/archimedean_spiral.py
+++ chiplotle/geometry/factory/archimedean_spiral.py
@@ -1,7 +1,7 @@
 from chiplotle.geometry.shapes.path import Path
 import math
 
-def archimedean_spiral(num_turns = 5, turn_spacing = 100, direction = "cw", exponent = 1.0, segments = 500):  
+def archimedean_spiral(num_turns = 5, turn_spacing = 100, direction = "cw", segments = 500):  
    '''
    Constructs an Archimedean (arithmetic) spiral with the given number of
    turns, turn spacing, direction ("cw" or "ccw"), exponent, and number of
@@ -10,7 +10,7 @@ def archimedean_spiral(num_turns = 5, turn_spacing = 100, direction = "cw", expo
    The default values will create a CW spiral with a right half that is
    500 (num_turns * turn_spacing) units from the center to the edge. 
    
-   r = b * theta^1/x
+   r = b * theta
    
    '''
 
@@ -18,8 +18,6 @@ def archimedean_spiral(num_turns = 5, turn_spacing = 100, direction = "cw", expo
    total_rads = two_pi * num_turns
    theta = 0.0
    theta_incr = total_rads / float(segments - 1)
-   
-   radius_multi = turn_spacing/two_pi
 
    if direction is "ccw":
       theta_incr *= -1.0
@@ -27,13 +25,13 @@ def archimedean_spiral(num_turns = 5, turn_spacing = 100, direction = "cw", expo
    spiral_points = []
    
    for i in range(segments):
-      r_width = radius_multi * (pow(theta, exponent)) 
-      r_height = radius_multi * (pow(theta, exponent))
-      x = math.cos(theta) * r_width;
-      y = math.sin(theta) * r_height;
+      
+      x = math.cos(theta) * ((turn_spacing * theta)/two_pi)
+      y = math.sin(theta) * ((turn_spacing * theta)/two_pi)
+
       spiral_points.append((x, y))
       theta += theta_incr
-
+      
    result = Path(spiral_points)
 
    return result
@@ -48,9 +46,9 @@ if __name__ == '__main__':
    from chiplotle.geometry.factory.line import line
    from chiplotle.tools import io
    
-   s = archimedean_spiral(exponent = .2)
+   s = archimedean_spiral()
    assert isinstance(s, Path)
- 
+   
    line_right = line((0,0), (500, 0))
    line_left = line((0,0), (-500, 0))
    line_up = line((0,0), (0, 500))
