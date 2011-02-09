ab254af1006ea8787d71674e668b22061af47a14
douglas <douglas@music.columbia.edu>
2011-02-09 04:46:35 +0000

    Changed spiral_archimedean.py to be a general Archimedean spiral. Can now do four main types of Archimedean spirals.

--- chiplotle/geometry/factory/spiral_archimedean.py
+++ chiplotle/geometry/factory/spiral_archimedean.py
@@ -1,37 +1,66 @@
 from chiplotle.geometry.shapes.path import Path
 import math
 
-def spiral_archimedean(num_turns = 5, turn_spacing = 100, direction = "cw", segments = 500):  
+def spiral_archimedean(num_turns = 5, wrapping_constant = 1, scaler = 15.0, direction = "cw", segments = 500):  
    '''
    Constructs an Archimedean (arithmetic) spiral with the given number of
-   turns, turn spacing, direction ("cw" or "ccw"), and number of
-   segments.
+   turns using the specified number of points
    
-   The default values will create a CW spiral with a right half that is
-   500 (num_turns * turn_spacing) units from the center to the edge. 
+   wrapping_constant controls how tightly the spiral is wound. Several classic
+   spirals can be created using different wrapping_constants:
    
-   r = b * theta
+   lituus:              -2
+   hyperbolic spiral:   -1
+   Archimedes' spiral:   1
+   Fermat's spiral:      2
+   
+   scaler controls how large the spiral is.
+   
+   The general Archimedean spiral equation is:
+   
+   r = a * theta^(1/n) 
+   
+   where a is the scaler and n is the wrapping_constant.
+   
+   
+   More info:
+   http://mathworld.wolfram.com/ArchimedeanSpiral.html
    
    '''
-
+   
    two_pi = math.pi * 2.0
    total_rads = two_pi * num_turns
    theta = 0.0
    theta_incr = total_rads / float(segments - 1)
-
-   if direction is "ccw":
-      theta_incr *= -1.0
    
+   #Spirals with a negative wrapping_constant technically begin at infinity,
+   #which obviously isn't practical. So we nudge theta by a bit to get a
+   #reasonable starting point
+   if wrapping_constant < 0.0:
+      theta += theta_incr
+
    spiral_points = []
    
+   exponent = 1.0/wrapping_constant
+   r = 0.0
+   
    for i in range(segments):
+      if exponent > 0:
+         r = scaler * math.pow(theta, exponent)
+      else:
+         pow = math.pow(theta, abs(exponent))
+         r = scaler * 1.0/pow
+         
+      x = math.cos(theta) * r
+      y = math.sin(theta) * r
       
-      x = math.cos(theta) * ((turn_spacing * theta)/two_pi)
-      y = math.sin(theta) * ((turn_spacing * theta)/two_pi)
-
+      if direction == "ccw":
+         y *= -1.0
+      #print "r: %f theta: %f" % (r, theta)
       spiral_points.append((x, y))
-      theta += theta_incr
       
+      theta += theta_incr   
+   
    result = Path(spiral_points)
 
    return result
@@ -42,16 +71,24 @@ def spiral_archimedean(num_turns = 5, turn_spacing = 100, direction = "cw", segm
 if __name__ == '__main__':
    from chiplotle.geometry.shapes.group import Group
    from chiplotle.geometry.factory.line import line
+   from chiplotle.geometry.transforms.offset import offset
+   from chiplotle.geometry.transforms.rotate import rotate
    from chiplotle.tools import io
    
-   s = spiral_archimedean()
-   assert isinstance(s, Path)
+   #one of each main spiral type
+   s1 = spiral_archimedean(wrapping_constant = 1, scaler = 15)
+   s2 = spiral_archimedean(wrapping_constant = 2, scaler = 85)
+   offset(s2, (0, -900))
    
-   line_right = line((0,0), (500, 0))
-   line_left = line((0,0), (-500, 0))
-   line_up = line((0,0), (0, 500))
-   line_down = line((0,0), (0, -500))
+   #these two are long, so we'll rotate them and move them to the side
+   #of the others
+   s3 = spiral_archimedean(wrapping_constant = -1, scaler = 100)
+   rotate(s3, math.pi/2.0)
+   offset(s3, (700, -1300))
+   s4 = spiral_archimedean(wrapping_constant = -2, scaler = 300)
+   rotate(s4, math.pi/2.0)
+   offset(s4, (1000, -1000))
    
-   g = Group([s, line_right, line_left, line_up, line_down])
+   g = Group([s1, s2, s3, s4])
 
    io.view(g)
