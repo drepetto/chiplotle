9286c38998e07935bdb0de5029d13c1cf7206066
douglas <douglas@music.columbia.edu>
2011-02-14 03:49:13 +0000

    Tweaked spiral_archimedean.py to allow radius argument.

--- chiplotle/geometry/factory/spiral_archimedean.py
+++ chiplotle/geometry/factory/spiral_archimedean.py
@@ -1,7 +1,7 @@
 from chiplotle.geometry.shapes.path import Path
 import math
 
-def spiral_archimedean(num_turns = 5, wrapping_constant = 1, scaler = 15.0, direction = "cw", segments = 500):  
+def spiral_archimedean(radius, num_turns = 5, wrapping_constant = 1, direction = "cw", segments = 500):  
    '''
    Constructs an Archimedean (arithmetic) spiral with the given number of
    turns using the specified number of points.
@@ -32,16 +32,19 @@ def spiral_archimedean(num_turns = 5, wrapping_constant = 1, scaler = 15.0, dire
    total_rads = two_pi * num_turns
    theta = 0.0
    theta_incr = total_rads / float(segments - 1)
+   exponent = 1.0/wrapping_constant
+   
+   scaler = float(radius) / math.pow(total_rads, exponent)
    
    #Spirals with a negative wrapping_constant technically begin at infinity,
    #which obviously isn't practical. So we nudge theta by a bit to get a
    #reasonable starting point
    if wrapping_constant < 0.0:
       theta += theta_incr
+      pow = math.pow(theta_incr, abs(exponent))
+      scaler = float(radius) / (1.0/pow)
 
    spiral_points = []
-   
-   exponent = 1.0/wrapping_constant
    r = 0.0
    
    for i in range(segments):
@@ -75,18 +78,19 @@ if __name__ == '__main__':
    from chiplotle.tools import io
    
    #one of each main spiral type
-   s1 = spiral_archimedean(wrapping_constant = 1, scaler = 15)
-   s2 = spiral_archimedean(wrapping_constant = 2, scaler = 85)
-   offset(s2, (0, -900))
+   s1 = spiral_archimedean(500, wrapping_constant = 1)
+   
+   s2 = spiral_archimedean(500, wrapping_constant = 2, direction = "ccw")
+   offset(s2, (0, -1000))
    
    #these two are long, so we'll rotate them and move them to the side
    #of the others
-   s3 = spiral_archimedean(wrapping_constant = -1, scaler = 100)
-   rotate(s3, math.pi/2.0)
-   offset(s3, (700, -1300))
-   s4 = spiral_archimedean(wrapping_constant = -2, scaler = 300)
-   rotate(s4, math.pi/2.0)
-   offset(s4, (1000, -1000))
+   s3 = spiral_archimedean(1800, wrapping_constant = -1, direction = "ccw")
+   rotate(s3, math.pi * 1.5)
+   offset(s3, (650, 400))
+   s4 = spiral_archimedean(1500, wrapping_constant = -2, direction = "ccw")
+   rotate(s4, math.pi * .6)
+   offset(s4, (1000, -1100))
    
    g = Group([s1, s2, s3, s4])
 
