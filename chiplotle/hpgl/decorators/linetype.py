99f37bf6dca95d98337cf3e064e9fd98359906fb
victor <vctradn@gmail.com>
2011-02-19 19:39:52 +0000

    New LineTypeDecorator available. Use to wrap Shapes with an HpGL line type. See hpgl/decorators/linetype.py

---
+++ chiplotle/hpgl/decorators/linetype.py
@@ -0,0 +1,34 @@
+from chiplotle.hpgl.commands import LT
+from chiplotle.core.interfaces.formatdecorator import FormatDecorator
+
+class LineTypeDecorator(FormatDecorator):
+   
+   def __init__(self, linetype, sticky=False):
+      FormatDecorator.__init__(self, sticky)
+      if not isinstance(linetype, LT):
+         raise TypeError
+      self.linetype = linetype
+
+   @property
+   def preformat_commands(self):
+      return [self.linetype]
+
+   @property
+   def postformat_commands(self):
+      return [LT( )]
+
+
+## DEMO
+if __name__ == '__main__':
+   from chiplotle.geometry.factory.rectangle import rectangle
+   from chiplotle.geometry.shapes.group import Group
+   from chiplotle.tools import io
+
+   ltd = LineTypeDecorator(LT(2, .5), False)
+   r1 = rectangle(1000, 200)
+   r2 = rectangle(300, 800)
+   ltd(r1)
+   print r1.format
+   g = Group([r1, r2])
+   print g.format
+   io.view(g)
