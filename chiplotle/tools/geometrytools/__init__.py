aee82e075df63b1ba87da3896dd7fb1e3574d2e1
victor <vctradn@gmail.com>
2011-02-20 04:45:50 +0000

    New geometrytools.get_shape_intersections(shape1, shape2) available. The function finds and returns all the points at which shape1 and shape2 intersect.

--- chiplotle/tools/geometrytools/__init__.py
+++ chiplotle/tools/geometrytools/__init__.py
@@ -1,1 +1,5 @@
+#from chiplotle.utils.imports.package_import import _package_import
+#_package_import(__path__[0], globals( ))
+
 from .get_centroid import get_centroid
+#from .get_line_intersection import get_line_intersection
