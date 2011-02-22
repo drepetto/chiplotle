49a89343e04379b192464d40b10bbe5df6e74d09
douglas <douglas@music.columbia.edu>
2011-02-22 02:06:31 +0000

    Added get_width_height.py convenience function to go with get_minmax.py

---
+++ chiplotle/tools/geometrytools/get_width_height.py
@@ -0,0 +1,35 @@
+from chiplotle.geometry.coordinate import Coordinate
+from chiplotle.geometry.coordinatearray import CoordinateArray
+from chiplotle.tools.geometrytools.get_minmax_coordinates import get_minmax_coordinates
+
+def get_width_height(coords):
+   '''Returns the width and height of the given `coords` list in a tuple.'''
+   
+   min_max = get_minmax_coordinates(coords.points)
+   
+   width = min_max[1][0] - min_max[0][0]
+   height = min_max[1][1] - min_max[0][1]
+   return (width, height)
+
+
+## DEMO
+if __name__ == '__main__':
+   from chiplotle.geometry.factory.circle import circle
+   from chiplotle.geometry.factory.random_walk_polar import random_walk_polar
+   from chiplotle.geometry.shapes.group import Group
+   from chiplotle.geometry.shapes.path import Path
+   from chiplotle.tools import io
+
+   #random points inside a circle
+   rw = random_walk_polar(100)
+
+   assert isinstance(rw, Path)
+   
+   h_w = get_width_height(rw)
+   
+   #a circle around all the points
+   c = circle(h_w[0]/2.0, 500)
+   
+   g = Group((rw, c))
+   
+   io.view(g)
