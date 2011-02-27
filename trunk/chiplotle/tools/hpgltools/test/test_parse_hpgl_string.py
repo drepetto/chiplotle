2fb0cc4a4e26d5aa538d8edc14d4a0b1fa30c886
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-02-27 03:43:06 +0000

    Added tests to hpgltools.parse_hpgl_string( ) test file.

--- trunk/chiplotle/tools/hpgltools/test/test_parse_hpgl_string.py
+++ trunk/chiplotle/tools/hpgltools/test/test_parse_hpgl_string.py
@@ -6,12 +6,24 @@ def test_parse_hpgl_string_01( ):
    t = hpgltools.parse_hpgl_string(s)
    assert t == ['IN', 'PA0,0', 'PD1,2', 'PU3,4', 'PD']
 
-
 def test_parse_hpgl_string_02( ):
    '''Coma separated floating point numbers are handled correctly.'''
    s = 'IN;PA0,0.1PD1.33,2.0;PU3,4PD'
    t = hpgltools.parse_hpgl_string(s)
    assert t == ['IN', 'PA0,0.1', 'PD1.33,2.0', 'PU3,4', 'PD']
+
+def test_parse_hpgl_string_03( ):
+   '''Negative values are handled correnctly.''' 
+   s = 'IN;SP2;PU;PA169.33,-0.00;PD;PA169.33,-1.00,170.22,6.32,170.86,12.67;PU;'
+   t = hpgltools.parse_hpgl_string(s)
+   assert t == ['IN', 'SP2', 'PU', 'PA169.33,-0.00', 'PD', 
+      'PA169.33,-0.00,170.22,6.32,170.86,12.67', 'PU']
+
+def test_parse_hpgl_string_04( ):
+   '''Commands stringed together (with no ; separator) are handled correctly.'''
+   s = 'INSP2PUPA2,-1PDRR100PU'
+   t = hpgltools.parse_hpgl_string(s)
+   assert t == ['IN', 'SP2', 'PU', 'PA2,-1', 'PD', 'RR100', 'PU']
 
 
 ## DCI (escape) commands ##
