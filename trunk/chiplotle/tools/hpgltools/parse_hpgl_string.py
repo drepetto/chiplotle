38729d4b1fe9b0efe0cf9c397c26406b6a56dffd
douglas <douglas@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-02-27 03:31:19 +0000

    Added NOTE to parse_hpgl_string.py explaining problem with ,- construction.

--- trunk/chiplotle/tools/hpgltools/parse_hpgl_string.py
+++ trunk/chiplotle/tools/hpgltools/parse_hpgl_string.py
@@ -31,6 +31,10 @@ def parse_hpgl_string(arg):
    ## TODO: Add all the supported escape (DCI) commands.
    dci_commands = ['\x1b\.\(', '\x1b\.Y']
 
+   # NOTE: added '-' to hpgl_patter because we were barfing on strings like
+   # 'PA1.99,-5.00;' -- the ,- construction wasn't being parsed correctly.
+   # Don't understand why normal negative integers were being parsed
+   # correctly though...
    hpgl_pattern = '[-0-9.,]*|'.join(hpgl_commands)
    dci_pattern = '[0-9.;]*|'.join(dci_commands)
    pattern = hpgl_pattern + "|" + dci_pattern
