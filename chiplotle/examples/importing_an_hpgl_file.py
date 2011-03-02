2996343b685f0c29f08b079314ab1ebb771588a0
douglas <douglas@music.columbia.edu>
2011-03-02 02:58:05 +0000

    Added examples/importing_an_hpgl_file.py and restored examples/media.

---
+++ chiplotle/examples/importing_an_hpgl_file.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+'''A simple example that shows how to load existing hpgl code into chiplotle.'''
+
+from chiplotle import *
+from chiplotle.tools.io.import_hpgl_file import import_hpgl_file
+from chiplotle.tools.plottertools import instantiate_virtual_plotter
+from chiplotle.geometry.shapes import *
+
+import time
+
+def main():
+
+    f = import_hpgl_file("./examples/media/square.hpgl")
+
+    print "\nHere are the contents of the file, expanded into a list of chiplotle objects:\n"
+    print f
+    
+    print "\nAnd here are the raw hpgl commands:\n"
+    
+    for c in f:
+        print c.format
+
+    # We can use io.view() to take a look...
+    
+    io.view(f)
+    
+    #small delay to give our external postscript reader time to load the output file
+    time.sleep(1)
+    
+    #We can also send the commands to a plotter. We'll use a virtual plotter in this example:
+    
+    plotter = instantiate_virtual_plotter()
+
+    #Now we'll send the contents of the file to the plotter so we can take a look.
+    plotter.write(f)
+    
+    #And now we'll use io.view() to view what the virtual plotter has drawn.
+    io.view(plotter)
+    time.sleep(1)
+
+    #We know from looking at the contents of the file above that the first thing
+    #the hpgl code does is select pen 1. Let's change that to pen 2:
+    
+    print "\nbefore:"
+    print f[0]
+
+    f[0] = SP(2)
+    
+    print "\nafter:"    
+    print f[0]
+    
+    #We'll clear the virtual plotter so that we can start a new drawing:
+    plotter.clear()
+    
+    plotter.write(f)
+    
+    #now we should see the same plot with a different color
+    io.view(plotter)
+    time.sleep(1)
+
+
+    #Now maybe I want to add a little somethingsomething:
+    
+    c = circle(1000)
+    
+    #insert the plotter into the list of commands at the position just before
+    #the last pen up (PU;) command:
+    f.insert(len(f) - 1, c)
+    
+    print "\nWe've inserted a circle (Path) into the list of commands:"
+    print f
+    
+    plotter.clear()
+    plotter.select_pen(3)
+    plotter.write(f)
+
+    io.view(plotter)
+   
+### run main if called from command line like so: 
+### $> python importing_an_hpgl_file.py
+if __name__ == '__main__': main()
