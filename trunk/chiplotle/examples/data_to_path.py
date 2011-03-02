4d51badfa37dcfc2b199a119f7b498579ddba7b0
douglas <douglas@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-03-02 04:49:47 +0000

    Added examples/data_to_path.py to show how to read in data and map it to a Chiplotle path.

---
+++ trunk/chiplotle/examples/data_to_path.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+'''Read in a data file and plot it using a virtual plotter.'''
+
+from chiplotle import *
+from chiplotle.tools.plottertools import instantiate_virtual_plotter
+from chiplotle.geometry import *
+
+def main():
+
+    plotter = instantiate_virtual_plotter(Coordinate(0, 0), Coordinate(30000, 20000))
+
+    data_file = open("./examples/media/indoor_temp.txt", 'r')
+    
+    data = data_file.readlines()
+    
+    print "read %d data points." % len(data)
+
+    #make an empty list and fill it with data points as coordinates
+    points = []
+    x = 0
+    
+    for y in data:
+        #we use rstrip() to remove the line break
+        y_value = eval(y.rstrip()) * 100
+        #print "y_value: %f" % y_value
+        c = Coordinate(x, y_value)
+        points.append(c)
+        x += 10
+        
+    data_path = Path(points)
+    
+    plotter.select_pen(4)
+    plotter.write(data_path)
+    
+    #let's offset it and plot it again!
+    
+    offset(data_path, (1000, 1000))
+    plotter.select_pen(5)
+    plotter.write(data_path)
+    
+    #let's draw a box around our offset plot
+    (min, max) = data_path.minmax_coordinates
+    (width, height) = max - min
+
+    r = rectangle(width, height)
+    
+    #a rectangle has its center at (0,0)
+    #so we shift it over so that its lower, left corner is (0,0)
+    #and we shift it some more to fit around our offset data
+    offset(r, (width/2, height/2))
+    offset(r, min)
+    
+    plotter.select_pen(6)
+    plotter.write(r)
+    
+    #take a looksee!
+    io.view(plotter)
+
+### run main if called from command line like so: 
+### $> python abstract_masterpiece.py
+if __name__ == '__main__': main()
+
