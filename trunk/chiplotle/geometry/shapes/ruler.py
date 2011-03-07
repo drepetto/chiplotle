c74c03b5a788cc6f1ad80c28621f47812415f438
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-03-07 02:12:07 +0000

    shapes.ruler( ) now has a `symmetric` argument. Set to True to distribute tick symmetrically centered along the invisible line of the ruler.

--- trunk/chiplotle/geometry/shapes/ruler.py
+++ trunk/chiplotle/geometry/shapes/ruler.py
@@ -5,7 +5,7 @@ from chiplotle.geometry.transforms.rotate import rotate
 from chiplotle.geometry.transforms.offset import offset
 import math
 
-def ruler(start_coord, end_coord, units, min_tick_height):
+def ruler(start_coord, end_coord, units, min_tick_height, symmetric=False):
    '''
    A measuring ruler.
    
@@ -13,6 +13,8 @@ def ruler(start_coord, end_coord, units, min_tick_height):
       to larger. e.g., (10, 20, 40).
    - `min_tick_height` is the height of the marks for the smallest units.
       The hight of the other units are multiples of this.
+   - `symmetric` set to True to draw the tick lines symmetrically around 
+      the invisible center-line.
    '''
    start_coord = Coordinate(*start_coord)
    end_coord = Coordinate(*end_coord)
@@ -24,8 +26,13 @@ def ruler(start_coord, end_coord, units, min_tick_height):
    for i, unit in enumerate(units):
       ticks = int(math.ceil(length / unit))
       for t in range(ticks):
-         x1, y1 = unit * t, 0
-         x2, y2 = unit * t, -min_tick_height * (i + 1)
+         tick_height = min_tick_height * (i + 1)
+         if symmetric:
+            x1, y1 = unit * t, tick_height / 2
+            x2, y2 = unit * t, -tick_height / 2
+         else:
+            x1, y1 = unit * t, 0
+            x2, y2 = unit * t, -tick_height
          tick = line((x1, y1), (x2, y2))
          result.append(tick)
    g = Group(result)
