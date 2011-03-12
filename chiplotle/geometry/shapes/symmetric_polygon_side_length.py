c798d657473f99d29399a406c07c9414b57dd430
victor <vctradn@gmail.com>
2011-03-12 22:56:08 +0000

    Added new shape `symmetric_polygon_side_length(count, length)`. Creates a symmetric polygon of `count` sides, each side of length `length`.

---
+++ chiplotle/geometry/shapes/symmetric_polygon_side_length.py
@@ -0,0 +1,10 @@
+from chiplotle.geometry.shapes import circle
+from chiplotle.geometry.transforms import scale
+
+def symmetric_polygon_side_length(count, length):
+   '''Creates a symmetric polygon with `count` sides, all with the 
+   same given `length`.
+   '''
+   result = circle(1.0, count)
+   scale(result, length / result.points.difference[0].magnitude)
+   return result
