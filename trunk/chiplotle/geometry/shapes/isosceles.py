5e4113ed90e36e8086088cd9a53f9f4df529d659
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-05-15 21:03:07 +0000

    isosceles() and arrow() can now take a 'filled' argument.

--- trunk/chiplotle/geometry/shapes/isosceles.py
+++ trunk/chiplotle/geometry/shapes/isosceles.py
@@ -1,12 +1,13 @@
-from chiplotle.geometry.core.path import Path
+#from chiplotle.geometry.core.path import Path
+from chiplotle.geometry.core.polygon import Polygon
 
-def isosceles(width, height):
+def isosceles(width, height, filled=False):
 
    tip = (0, height)
    left = (- width / 2.0, 0)
    right = (width / 2.0, 0)
 
-   return Path([tip, left, right, tip])
+   return Polygon([tip, left, right, tip], filled = filled)
 
 
 
@@ -14,7 +15,6 @@ def isosceles(width, height):
 
 if __name__ == '__main__':
    from chiplotle.tools import io
-   e = isosceles(10, 20)
-   assert isinstance(e, Path)
+   e = isosceles(200, 400, filled=True)
    print e.format
    io.view(e)
