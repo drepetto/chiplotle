9e415a6617ee5318fe4870059b0a6afdcc7315f1
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-05-21 20:07:42 +0000

    Fixed bug in radial ruler: start angle was not being set correctly.

--- trunk/chiplotle/geometry/shapes/radial_ruler.py
+++ trunk/chiplotle/geometry/shapes/radial_ruler.py
@@ -19,11 +19,11 @@ def radial_ruler(radius,
       for t in range(ticks):
          tick_height = min_tick_height * (i + 1)
          if symmetric:
-            r1, a1 =  tick_height / 2, unit * t
-            r2, a2 = -tick_height / 2, unit * t 
+            r1, a1 =  tick_height / 2, unit * t + start_angle
+            r2, a2 = -tick_height / 2, unit * t + start_angle
          else:
-            r1, a1 = 0, unit * t
-            r2, a2 = -tick_height, unit * t
+            r1, a1 = 0, unit * t + start_angle
+            r2, a2 = -tick_height, unit * t + start_angle
          xy1 = p2c(Coordinate(r1, a1) + Coordinate(radius, 0)) 
          xy2 = p2c(Coordinate(r2, a2) + Coordinate(radius, 0)) 
          tick = line(xy1, xy2)
@@ -35,7 +35,7 @@ if __name__ == '__main__':
    from chiplotle import * 
    import math
       
-   r1 = radial_ruler(1000, 0, math.pi, (math.pi/20, math.pi/10), 20, True)
+   r1 = radial_ruler(1000, 0.1, math.pi, (math.pi/20, math.pi/10), 20, True)
    r2 = radial_ruler(500, math.pi/3, math.pi, (math.pi/40, math.pi/10), 20, False)
    c = circle(30)
    g = Group([r1, r2, c])
