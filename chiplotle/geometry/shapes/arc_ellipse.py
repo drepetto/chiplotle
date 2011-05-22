c83391e7b8e8be124ce00a2501dc3a7f17c269cb
victor <vctradn@gmail.com>
2011-05-22 16:22:40 +0000

    Added `segmentation_mode` to arc_ellipse() and arc_circle(). Can be set to either '2pi' or 'arc'. the number of segments will be computed on either the whole circle or just the arc segment.

--- chiplotle/geometry/shapes/arc_ellipse.py
+++ chiplotle/geometry/shapes/arc_ellipse.py
@@ -1,53 +1,56 @@
+from chiplotle.geometry.core.coordinate import Coordinate
 from chiplotle.geometry.core.path import Path
-from chiplotle.geometry.core.group import Group
-
 import math
 
-def arc_ellipse(width, height, start_angle, end_angle, segments = 100):  
+def arc_ellipse(width, height, 
+                start_angle, end_angle, 
+                segments = 100, segmentation_mode = '2PI'):  
    '''
    Constructs an arc from an ellipse with the given width, height,
    and number of segments. Arc goes from start_angle to end_angle,
    both of which are in radians.
+
+      - `segmentation_mode` : '2PI' or 'arc'. The first segments
+         the whole ellipse into the given number of segments,
+         the second segments the arc.
    '''
    if start_angle > end_angle:
       end_angle += math.pi * 2
-      
-   two_pi = math.pi * 2.0
    
-   rads_incr = two_pi / float(segments)
-   half_width = width * 0.5
-   half_height = height * 0.5
-   
+   def _divide_2pi():
+      return (math.pi * 2) / float(segments)
+   def _divide_arc():
+      return (end_angle - start_angle) / float(segments)
+
+   segmentation_map = {'2pi' : _divide_2pi,
+                       'arc' : _divide_arc}
+
+   rads_incr = segmentation_map[segmentation_mode.lower()]()
    rads = start_angle
-   
-   arc_points = []
-   
-   while rads < end_angle: 
-      sin = math.sin(rads);
-      cos = math.cos(rads);
-
-      point_x = (half_width * cos);
-      point_y = (half_height * sin);
-
-      arc_points.append((point_x, point_y))
-      
+   arc = []
+   while rads < end_angle:
+      coord = Coordinate(math.cos(rads), math.sin(rads))
+      coord = coord * Coordinate(width / 2.0, height / 2.0)
       rads += rads_incr
-   ## append final point to get exact end_angle... 
-   arc_points.append((half_width * math.cos(end_angle), 
-                      half_height * math.sin(end_angle)))
-   return Path(arc_points)
+      arc.append(coord)
+   ## NOTE: this is better than using rads <= end_angle since
+   ## the equality does not always work as expected with floats
+   ## due to rounding.
+   last = Coordinate(math.cos(end_angle), math.sin(end_angle))
+   last *= Coordinate(width / 2.0, height / 2.0)
+   arc.append(last)
+   return Path(arc)
 
 
 ## RUN DEMO CODE
 
 if __name__ == '__main__':
    from chiplotle.tools import io
+   from chiplotle.geometry.core.group import Group
    
    gr = Group()
-   
    for radius in range(100, 1000, 10):
-       ae = arc_ellipse(radius, radius * 2, 1.0, math.pi)
-       assert isinstance(ae, Path)
+       ae = arc_ellipse(radius, radius * 2, 0, math.pi/2, 5, 'arc')
        gr.append(ae)
 
    io.view(gr)
