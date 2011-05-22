8ccba8b6f540964d0c7aed5dcb306802a554609a
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-05-22 16:38:34 +0000

    Simplified code in shapes.circle() and shapes.ellipse().

--- trunk/chiplotle/geometry/shapes/circle.py
+++ trunk/chiplotle/geometry/shapes/circle.py
@@ -1,15 +1,18 @@
-from chiplotle.geometry.shapes.ellipse import ellipse
+from chiplotle.geometry.core.polygon import Polygon
+from chiplotle.geometry.transforms.scale import scale
+import math
 
 def circle(radius, segments = 36):
-   return ellipse(radius * 2, radius * 2, segments)
+   '''Returns a circle.'''
+   coords = [(math.cos(math.pi * 2 / segments * r) * radius, 
+              math.sin(math.pi * 2 / segments * r) * radius) 
+              for r in range(segments)]
+   return Polygon(coords)
 
 
 ## RUN DEMO CODE
 
 if __name__ == '__main__':
    from chiplotle.tools import io
-   from chiplotle.geometry.core.path import Path
-   e = circle(10)
-   assert isinstance(e, Path)
-   print e.format
+   e = circle(1000)
    io.view(e)
