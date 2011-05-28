496ced65392e009e1171e7b47a937bbd8505a74d
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-05-28 14:31:18 +0000

    Cleaned up transforms.perpendicular_displace.

--- trunk/chiplotle/geometry/transforms/perpendicular_displace.py
+++ trunk/chiplotle/geometry/transforms/perpendicular_displace.py
@@ -13,34 +13,41 @@ def perpendicular_displace(path, displacements):
    if not isinstance(path, Path):
       raise TypeError('`path` should be of type Path.')
    if len(path.points) != len(displacements):
-      raise ValueError('len(path) == len(displacements) must be true.')
+      raise ValueError('len(path) != len(displacements).')
 
+   ## TODO may want to check here for identical consecutive coords
+   ## to avoid 0 differences.
    result = [ ]
    d_points = difference(path.points)
-   for i in range(len(path.points) - 1):
-      perp = ~d_points[i]
-      mag = perp.magnitude
-      if mag == 0: ## two points are overlapping.
-         disp = 0
-      else:
-         disp = perp / mag * displacements[i]
+   d_points.append(d_points[-1])
+   for i in range(len(path.points)):
+      perp = d_points[i].perpendicular.normalized
+      disp = perp * displacements[i]
+#      perp = d_points[i].perpendicular
+#      mag = perp.magnitude
+#      if mag == 0: ## two points are overlapping.
+#         disp = 0
+#      else:
+#         disp = perp / mag * displacements[i]
 
       result.append(path.points[i] + disp)
    path.points = Path(result).points
 
 
-## demo
+## ~~~~~~~~~~~~~~~ demo ~~~~~~~~~~~
 if __name__ == '__main__':
    from chiplotle import *
    import copy
    import random
+   import math
 
-   rndpath = [random.randint(0, 2000) for i in range(10)]
+   rndpath = [random.randint(0, 2000) for i in range(2 * 4)]
    p = bezier_path(rndpath, .1, 80)
+   print '*** ', p
    pc = copy.deepcopy(p)
    PenDecorator(Pen(2))(pc)
 
-   d = [math.sin(x / 3.14159) * 30 for x in range(len(p.points))]
+   d = [math.sin(x / math.pi) * 30 for x in range(len(p.points))]
 
    perpendicular_displace(p, d)
    io.view(group([p, pc]))
