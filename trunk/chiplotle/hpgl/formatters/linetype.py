06244a69199a914a89c6ff30b97207262c97e922
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-07-09 15:07:38 +0000

    Changed example code in hpgl.formatters.linetype.

--- trunk/chiplotle/hpgl/formatters/linetype.py
+++ trunk/chiplotle/hpgl/formatters/linetype.py
@@ -15,19 +15,27 @@ class LineType(FormatDecorator):
       return [LT(self.linetype, self.length)]
 
 
-
 ## DEMO
 if __name__ == '__main__':
-   from chiplotle.geometry.shapes.rectangle import rectangle
-   from chiplotle.geometry.core.group import Group
-   from chiplotle.tools import io
+   from chiplotle import *
 
-   r1 = rectangle(1000, 200)
-   r2 = rectangle(300, 800)
-   ltd = LineType(2, .5)
-   ltd(r1)
-   g = Group([r1, r2])
-   ltd = LineType(3, .1)
-   ltd(g)
-   print g.format
-   io.view(g)
+   lines = Group()
+   for i in range(10):
+      dot_space = i / 2.0
+      line_space = i * 400
+
+      lt = formatters.LineType(1, dot_space)
+      l = line((0, line_space), (3000, line_space))
+      lt(l)
+
+      lb = label(str(dot_space), .1, .1)
+      offset(lb, (-10, line_space))
+
+      lines.append(l)
+      lines.append(lb)
+
+   formatters.Pen(1)(lines)
+   offset(lines, (0, 1000))
+   io.view(lines)
+   #p = plottertools.instantiate_plotters()[0]
+   #p.write(lines)
