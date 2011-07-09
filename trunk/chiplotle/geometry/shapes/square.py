fe31f9031acbec042d3d70e9fa607bc187b0cd60
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2011-07-09 15:06:46 +0000

    Added square(x) shapes.

---
+++ trunk/chiplotle/geometry/shapes/square.py
@@ -0,0 +1,5 @@
+from chiplotle.geometry.shapes.rectangle import rectangle
+
+def square(width_height):
+   '''Returns a square centered at (0, 0).'''
+   return rectangle(width_height, width_height)
