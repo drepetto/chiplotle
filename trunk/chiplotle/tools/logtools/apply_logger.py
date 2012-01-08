b5e27444269c1ae58d7aa88a8ab7e740db3d6484
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2012-01-08 00:34:28 +0000

    Refactoring.

--- trunk/chiplotle/tools/logtools/apply_logger.py
+++ trunk/chiplotle/tools/logtools/apply_logger.py
@@ -6,19 +6,3 @@ def apply_logger(f):
    f.logger = logger
    return f
 
-
-#class applyLogger(object):
-#
-#   def __init__(self, f):
-#      self.f = f
-#      # need this?
-#      #self.__name__ = f.__name__
-#
-#
-#   def __call__(self):
-#      from chiplotle.tools.logtools.get_logger import get_logger
-#      logger = get_logger(f.func_name)
-#      ## how would f access logger from within itself?
-#      f.logger = logger
-#      self.f( )
-      
