1f2e7b67e6e190a6f54f0f36e13d4b0d693b6ae2
victor <victor@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2012-01-16 00:09:54 +0000

    Removed deprecated examples.

--- trunk/chiplotle/examples_DEPRECATED/arrow_decorator.py
+++
@@ -1,8 +0,0 @@
-from chiplotle import *
-from chiplotle.geometry.core.path import Path
-from chiplotle.geometry.shapes.arrow import arrow
-
-line = Path([(0, 0), (1000, 1000), (-1000, 1000), (0, 2000)])
-a = arrow(line, 500, 1000)
-
-io.view(a)
--- trunk/chiplotle/examples_DEPRECATED/round_rectangle.py
+++
@@ -1,8 +0,0 @@
-from chiplotle import *
-
-r = RoundRectangle((-500, 500), 1000, 2000, weight=3)
-
-plotter = instantiate_plotters( )[0]
-plotter.write(r)
-
-
