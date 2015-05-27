351ef9145211504a8203dec87cfae7e3ad126783
douglas <douglas@2aec33f6-6f3a-0410-a3fa-bf705c96ab94>
2015-05-27 02:19:37 +0000

    No, really, this time it's fixed.

--- trunk/chiplotle/tools/hpgltools/pens_updown_to_papr.py
+++ trunk/chiplotle/tools/hpgltools/pens_updown_to_papr.py
@@ -12,33 +12,42 @@ def pens_updown_to_papr(lst):
         raise TypeError('`lst` argument must be a list or tuple.')
 
     result = [ ]
-    last_penplot = None
+    last_move = None
     
     for e in lst:
         if isinstance(e, (PU, PD)):
             if len(e.xy) > 0:
-                if last_penplot is None:
+                if last_move is None:
                     msg = "*** WARNING: %s with coordinates found without prior PA or PR. PA assumed." % e
                     print(msg)
-                    last_penplot = PA( )
+                    last_move = PA( )
+
+                new_move = None
                 
+                if isinstance(last_move, PA):
+                    new_move = PA()
+                elif isinstance(last_move, PR):
+                    new_move = PR()
+                    
+                new_move.xy = e.xy
+                
+                up_down_command = None
                 if isinstance(e, PU):
-                    last_penplot = PU( )
+                    up_down_command = PU( )
                 elif isinstance(e, PD):
-                    last_penplot = PD( )
+                    up_down_command = PD( )
                 
-                last_penplot.xy = e.xy
-                new_coord = copy.deepcopy(e)
-                new_coord.xy = None
-                result.append(new_coord)
-                result.append(last_penplot)
+                result.append(up_down_command)
+                result.append(new_move)
+                
+                last_move = new_move
             else:
                 result.append(e)
         else:
             if isinstance(e, PR):
-                last_penplot = PR( )
+                last_move = PR( )
             elif isinstance(e, PA):
-                last_penplot = PA( )
+                last_move = PA( )
             result.append(e)
 
     return result
