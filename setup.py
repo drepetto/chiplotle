39b14412ee69fb9c7f0a650b937bb68886951e89
Víctor Adán <contact@victoradan.net>
2015-05-28 23:04:04 -0400

    Some installation/setup/testing cleanup
    - Removed ancient ez_install stuff from setup.py
    - Added max versions to dependencies
    - Added .gitignore file
    - Added requirements-dev.txt with pytests requirement to run tests
    - Added test.sh to run tests

--- setup.py
+++ setup.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#from distutils.core import setup
-from ez_setup import use_setuptools
-use_setuptools( ) # <-- this must come before setup import
 from setuptools import setup, find_packages
 
-setup(name              = 'Chiplotle', 
-      version           = '0.4.1', 
+setup(name              = 'Chiplotle',
+      version           = '0.4.2',
       description       = 'Chiplotle is an HPGL Python API.',
       long_description  = 'Chiplotle is an HPGL Python API.',
       author            = 'Víctor Adán and Douglas Repetto',
       author_email      = 'chiplotle@music.columbia.edu',
       url               = 'http://music.columbia.edu/cmc/chiplotle',
       keywords          = 'vector graphics hpgl plotter plot pen',
-      license           = 'GPL', 
+      license           = 'GPL',
 
       include_package_data = True,
-      packages             = ['chiplotle'], #find_packages( ),
-      install_requires     = ['pyserial', 'numpy'],
+      packages             = ['chiplotle'],
+      install_requires     = ['pyserial<=3', 'numpy<=2'],
       entry_points         = {'console_scripts':
          ['chiplotle = chiplotle.core.cfg._run_chiplotle:_run_chiplotle',]},
-      scripts              = [ 
+      scripts              = [
          'chiplotle/scripts/envelope.py',
          'chiplotle/scripts/find_hpgl_file_dimensions.py',
          'chiplotle/scripts/plot_hpgl_file_max_size.py',
@@ -29,8 +26,6 @@ setup(name              = 'Chiplotle',
          'chiplotle/scripts/plot_hpgl_file_virtual.py',
          'chiplotle/scripts/typewriter.py',
          'chiplotle/scripts/view_hpgl_file.py',
-         ], 
+         ],
       )
-
-# just testing git
 
