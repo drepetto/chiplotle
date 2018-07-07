537f60448b843065449ed6d1a6b5a528d8856f2b
Victor Adan <vctradn@gmail.com>
2018-07-06 21:40:50 -0500

    Revert "Tweaked pens_updown_to_papr.py so that it removes redundant PU and PD commands."
    This reverts commit b86000bf39b044996166c73dfce099f975625b9c.
    
    Revert "No, really, this time it's fixed."
    
    This reverts commit a90a8ce3f5083580403d65f16d35e7128d0e3584.
    
    Revert "Whoops, the bug in pens_updown_to_papr.py wasn't quite fixed. Now it is."
    
    This reverts commit 71bdf46ef3945c5eed5877ca4f1d42e19032c556.
    
    Revert "fixed bug in pens_updown_to_papr.py that was overwriting contents of list passed to function."
    
    This reverts commit 35151f9f6fc64e9b350fab1a1191a8cb92273bc7.

--- chiplotle/tools/hpgltools/pens_updown_to_papr.py
+++ chiplotle/tools/hpgltools/pens_updown_to_papr.py
@@ -1,5 +1,5 @@
 from chiplotle.hpgl.commands import PA, PR, PU, PD
-from chiplotle.geometry.core.coordinate import Coordinate
+import copy
 
 def pens_updown_to_papr(lst):
     '''Converts all PU((x1, y1, x2, y2) and PD(x1, y1, x2, y2) found in `lst`
@@ -11,63 +11,25 @@ def pens_updown_to_papr(lst):
         raise TypeError('`lst` argument must be a list or tuple.')
 
     result = [ ]
-    last_move = None
-    
-    pen_down = False
-    pen_up = True
-    
+    last_penplot = None
     for e in lst:
         if isinstance(e, (PU, PD)):
-                
             if len(e.xy) > 0:
-                if last_move is None:
+                if last_penplot is None:
                     msg = "*** WARNING: %s with coordinates found without prior PA or PR. PA assumed." % e
                     print(msg)
-                    last_move = PA( )
-
-                new_move = None
-                
-                if isinstance(last_move, PA):
-                    new_move = PA()
-                elif isinstance(last_move, PR):
-                    new_move = PR()
-                    
-                new_move.xy = e.xy
-                
-                up_down_command = None
-                
-                if isinstance(e, PU):
-                    if pen_down:
-                        up_down_command = PU( )
-                        result.append(up_down_command)
-                        pen_up = True
-                        pen_down = False
-                        
-                elif isinstance(e, PD):
-                    if pen_up:
-                        up_down_command = PD( )
-                        result.append(up_down_command)
-                        pen_down = True
-                        pen_up = False
-
-                    
-                result.append(new_move)
-                
-                last_move = new_move
+                    last_penplot = PA( )
+                last_penplot.xy = e.xy
+                e.xy = None
+                result.append(e)
+                result.append(copy.copy(last_penplot))
             else:
-                if isinstance(e, PU):
-                    if pen_down:
-                        result.append(e)
-                        pen_up = True
-                elif isinstance(e, PD):
-                    if pen_up:
-                        result.append(e)
-                        pen_down = True
+                result.append(e)
         else:
             if isinstance(e, PR):
-                last_move = PR( )
+                last_penplot = PR( )
             elif isinstance(e, PA):
-                last_move = PA( )
+                last_penplot = PA( )
             result.append(e)
 
     return result
