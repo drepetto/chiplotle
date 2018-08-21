9e8d89ecc5cf3b512caf184d4e66ed5a4e84b4b8
Will Price <will.price94@gmail.com>
2018-08-21 23:15:16 +0100

    Fix last instance of isinstance check that uses str

--- chiplotle/tools/serialtools/what_plotter_in_port.py
+++ chiplotle/tools/serialtools/what_plotter_in_port.py
@@ -3,6 +3,8 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 from future import standard_library
+from six import text_type, string_types
+
 standard_library.install_aliases()
 import serial
 import time
@@ -17,7 +19,7 @@ def what_plotter_in_port(port, wait_time=10):
     Returns the ID of the plotter found or None.'''
 
     assert isinstance(wait_time, int)
-    if not isinstance(port, str):
+    if not isinstance(port, (string_types, text_type)):
         raise TypeError('`port` must be a string.')
 
     from chiplotle.tools.serialtools import instantiate_serial_from_config_file
