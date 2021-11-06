eb5295492743650ae4157d308e7aebf4acfcd1b1
Xoi Ga <xoiga123@outlook.com.vn>
2021-11-07 03:36:23 +0700

    Importing LB, BL, DT commands patch

--- chiplotle/tools/hpgltools/parse_hpgl_string.py
+++ chiplotle/tools/hpgltools/parse_hpgl_string.py
@@ -1,5 +1,6 @@
 import re
 
+
 def parse_hpgl_string(arg):
     '''The function takes a string `arg` of HPGL commands, parses them
     (separates them) and returns them in a list.
@@ -7,28 +8,34 @@ def parse_hpgl_string(arg):
     if not isinstance(arg, str):
         raise TypeError('`arg` must be of type string.')
 
-    string_commands    = ['LB', 'DT']
-    numeric_commands  = ['AA','AF','AH','AP','AR','AS',
-        'BF','BL',
-        'CA','CC','CI','CM','CP','CS','CT','CV',
-        'DC','DF','DI','DP','DR','DS','DV', #DL
-        'EA','EC','EP','ER','ES','EW',
-        'FP','FR','FS','FT',
-        'GC', #GM GP
-        'IM','IN','IP','IV','IW',
-        'KY',
-        'LO','LT',
-        'NR',
-        'OA','OC','OD','OE','OF','OG','OH','OI','OK','OL','OO','OP','OS','OT','OW',
-        'PA','PB','PD','PG','PM','PR','PS','PT','PU',
-        'RA','RO','RR',
-        'SA','SC','SI','SL','SM','SP','SR','SS', #SG
-        'TL',
-        #'UC', UF
-        'VS',
-        'WD','WG',
-        'XT',
-        'YT']
+    # find label terminator first (DT)
+    try:
+        terminator = re.search('DT(.*?)(,[01])?;', arg).group(1)
+    except:
+        terminator = '\x03'  # EOT
+
+    string_commands = ['LB', 'BL']
+    numeric_commands = ['AA', 'AF', 'AH', 'AP', 'AR', 'AS',
+                        'BF',
+                        'CA', 'CC', 'CI', 'CM', 'CP', 'CS', 'CT', 'CV',
+                        'DC', 'DF', 'DI', 'DP', 'DR', 'DS', 'DV',  # DL
+                        'EA', 'EC', 'EP', 'ER', 'ES', 'EW',
+                        'FP', 'FR', 'FS', 'FT',
+                        'GC',  # GM GP
+                        'IM', 'IN', 'IP', 'IV', 'IW',
+                        'KY',
+                        'LO', 'LT',
+                        'NR',
+                        'OA', 'OC', 'OD', 'OE', 'OF', 'OG', 'OH', 'OI', 'OK', 'OL', 'OO', 'OP', 'OS', 'OT', 'OW',
+                        'PA', 'PB', 'PD', 'PG', 'PM', 'PR', 'PS', 'PT', 'PU',
+                        'RA', 'RO', 'RR',
+                        'SA', 'SC', 'SI', 'SL', 'SM', 'SP', 'SR', 'SS',  # SG
+                        'TL',
+                        # 'UC', UF
+                        'VS',
+                        'WD', 'WG',
+                        'XT',
+                        'YT']
     ## TODO: Add all the supported escape (DCI) commands.
     dci_commands = ['\x1b\.\(', '\x1b\.Y']
 
@@ -36,14 +43,14 @@ def parse_hpgl_string(arg):
     # 'PA1.99,-5.00;' -- the ,- construction wasn't being parsed correctly.
     # Don't understand why normal negative integers were being parsed
     # correctly though...
-    # TODO: the whole parser needs to be rewritten. This is simple,
-    #         but inaccurate, especially for DT/LB commands.
-    numeric_pattern= '|'.join([c + '[-0-9.,]*' for c in numeric_commands])
-    dci_pattern     = '|'.join([c + '[0-9.;]*' for c in dci_commands])
-    string_pattern = '|'.join([c + '[^;]+' for c in string_commands])
+    # NOTE: some files have whitespace as separator,
+    # even though that's not the official grammar
+    numeric_pattern = '|'.join([c + '[-0-9., ]*' for c in numeric_commands])
+    dci_pattern = '|'.join([c + '[0-9.;]*' for c in dci_commands])
+    string_pattern = '|'.join([c + '.*?[%s]' % terminator for c in string_commands])
     pattern = '|'.join([numeric_pattern, string_pattern, dci_pattern])
     ## this assumes that the re will find each and every hpgl command
     ## with the pattern. Any command not matched will effectively be
     ## removed... we don't want that.
     result = re.findall(pattern, arg)
-    return result
+    return ['DT' + terminator] + result
